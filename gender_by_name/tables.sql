782c5da838903e1178bed6826e42aaeb72cfeec8
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2019-04-26 17:25:22 +0300

    Create tables.sql

---
+++ gender_by_name/tables.sql
@@ -0,0 +1,25 @@
+create table gender_by_ending
+(
+    id        serial      not null constraint gender_by_ending_pkey primary key,
+    ending    varchar(50) not null,
+    gender    gender      not null,
+    name_type name_type   not null,
+    example   varchar(255)
+);
+comment on table gender_by_ending is 'Определение пола по окончаниям  фамилий, отчеств';
+comment on column gender_by_ending.ending is 'Окончание';
+comment on column gender_by_ending.gender is 'Пол';
+comment on column gender_by_ending.name_type is 'Тип last_name или middle_name';
+comment on column gender_by_ending.example is 'Пример';
+create index gender_by_ending_name_type_index on gender_by_ending (name_type);
+
+create table person_name_dictionary
+(
+    id     serial       not null constraint person_name_dictionary_pkey primary key,
+    name   varchar(255) not null,
+    gender gender       not null
+);
+comment on table person_name_dictionary is 'Словарь имен с полом';
+comment on column person_name_dictionary.name is 'Имя';
+comment on column person_name_dictionary.gender is 'Пол';
+create unique index person_name_dictionary_name_uindex on person_name_dictionary (lower(name::text));
