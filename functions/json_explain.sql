2fb5cffafdd81d342d1c6006e06e314b6d76d97d
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2020-02-20 00:06:28 +0300

    Create json_explain.sql

---
+++ functions/json_explain.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION json_explain(
+    query TEXT,
+    params TEXT[] DEFAULT ARRAY[]::text[]
+) RETURNS SETOF JSON AS $$
+BEGIN
+    RETURN QUERY
+    EXECUTE 'EXPLAIN ('
+         || ARRAY_TO_STRING(ARRAY_APPEND(params, 'FORMAT JSON'), ',')
+         || ')'
+         || query;
+END
+$$ LANGUAGE plpgsql;
+
+SELECT json_explain('SELECT * FROM pg_class', ARRAY['ANALYSE'])->0;
