310f3b86414c1f9bc061d72a2b9a20660391b366
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2020-02-20 00:23:14 +0300

    Create jsonb_diff.sql

---
+++ functions/jsonb_diff.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION jsonb_diff(l JSONB, r JSONB) RETURNS JSONB AS $json_diff$
+    SELECT jsonb_object_agg(a.key, a.value)
+    FROM (SELECT key, value FROM jsonb_each(l)) AS a(key,value)
+    LEFT OUTER JOIN (SELECT key, value FROM jsonb_each(r)) b(key,value) ON a.key = b.key
+    WHERE a.value != b.value OR b.key IS NULL;
+$json_diff$
+LANGUAGE sql;
+
+SELECT jsonb_diff('{"a":1,"b":2}'::JSONB, '{"a":1,"b":null}'::JSONB);
