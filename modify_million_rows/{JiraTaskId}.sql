4157afa59549ff059258a424fcd9e3edf9637d43
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2020-08-18 16:06:12 +0300

    актуализоровал про IF [NOT] EXISTS

--- modify_million_rows/{JiraTaskId}.sql
+++ modify_million_rows/{JiraTaskId}.sql
@@ -6,12 +6,13 @@ VACUUM VERBOSE ANALYZE {table};
 -- подготавливаем в таблице данные для последующего обновления: id, поля со старыми и/или новыми данными (при необходимости)
 -- или подготавливаем в таблице данные для последующего удаления: id и все остальные поля
 -- таблица является резервной копией для возможности отката
-CREATE TABLE IF NOT EXISTS {table}_{JiraTaskId} AS
+DROP TABLE IF EXISTS {table}_{JiraTaskId};
+CREATE TABLE {table}_{JiraTaskId} AS
 SELECT id/*значения в колонке id должны быть уникальными!*/, ...
 FROM {table}, ...
 WHERE ...;
  
-CREATE UNIQUE INDEX IF NOT EXISTS {table}_{JiraTaskId}_uniq_id ON {table}_{JiraTaskId} (id);
+CREATE UNIQUE INDEX {table}_{JiraTaskId}_uniq_id ON {table}_{JiraTaskId} (id);
  
 --SELECT * FROM {table}_{JiraTaskId} ORDER BY id LIMIT 100; -- для отладки
  
