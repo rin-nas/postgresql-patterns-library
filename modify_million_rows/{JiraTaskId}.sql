81bf1a20cd28ba14c54cb9ced01afa7254f8aab1
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2020-09-04 00:58:59 +0300

    Update {JiraTaskId}.sql

--- modify_million_rows/{JiraTaskId}.sql
+++ modify_million_rows/{JiraTaskId}.sql
@@ -1,21 +1,5 @@
 -- Запросы выполнять НЕ в транзакции!
- 
-VACUUM VERBOSE ANALYZE {table};
- 
--- сначала выполняем все тяжёлые вычисления
--- подготавливаем в таблице данные для последующего обновления: id, поля со старыми и/или новыми данными (при необходимости)
--- или подготавливаем в таблице данные для последующего удаления: id и все остальные поля
--- таблица является резервной копией для возможности отката
-DROP TABLE IF EXISTS {table}_{JiraTaskId};
-CREATE TABLE {table}_{JiraTaskId} AS
-SELECT id/*значения в колонке id должны быть уникальными!*/, ...
-FROM {table}, ...
-WHERE ...;
- 
-CREATE UNIQUE INDEX {table}_{JiraTaskId}_uniq_id ON {table}_{JiraTaskId} (id);
- 
---SELECT * FROM {table}_{JiraTaskId} ORDER BY id LIMIT 100; -- для отладки
- 
+
 DO $$
 DECLARE
     -- private variables (do not edit):
@@ -117,5 +101,3 @@ BEGIN
  
 END
 $$;
- 
-VACUUM VERBOSE ANALYZE {table};
