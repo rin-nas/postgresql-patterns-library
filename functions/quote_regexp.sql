f4209a1fd6c65ec23fb2da2b4704dc09595efb4f
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2020-09-04 15:04:30 +0300

    Update quote_regexp.sql

--- functions/quote_regexp.sql
+++ functions/quote_regexp.sql
@@ -1,3 +1,4 @@
+-- https://stackoverflow.com/questions/5144036/escape-function-for-regular-expression-or-like-patterns/45741630#45741630
 create function quote_regexp(text) returns text
     stable
     language plpgsql
