fe7d05cd405aa7e4fe0caaf977048cfeec66b3af
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2020-09-04 15:00:03 +0300

    Update quote_like.sql

--- functions/quote_like.sql
+++ functions/quote_like.sql
@@ -2,7 +2,11 @@ create function quote_like(text) returns text
     immutable
     strict
     language sql
+    PARALLEL SAFE -- Postgres 10 or later
 as
 $$
-SELECT replace(replace(replace($1, '\', '\\'), '_', '\_'), '%', '\%');
+SELECT replace(replace(replace($1
+                               , '\', '\\') -- must come 1st
+                               , '_', '\_')
+                               , '%', '\%');
 $$;
