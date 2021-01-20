e6f3e24b6d363c2b12151796ed60dee8e6bb09bc
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-01-20 17:02:26 +0300

    Update {JiraTaskId}_finish.sql

--- modify_million_rows/{JiraTaskId}_finish.sql
+++ modify_million_rows/{JiraTaskId}_finish.sql
@@ -1,1 +1,5 @@
 VACUUM VERBOSE ANALYZE {table};
+
+DROP TABLE IF EXISTS {table}_{JiraTaskId};
+-- или, если нужно удалить временную таблицу позже, то переносим в другую схему
+ALTER TABLE IF EXISTS {table}_{JiraTaskId} SET SCHEMA migration;
