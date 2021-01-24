30b07600d1e26872f3aeb23e569e7a79ea17bda6
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-01-24 22:52:35 +0300

    Update {JiraTaskId}.sh

--- modify_million_rows/{JiraTaskId}.sh
+++ modify_million_rows/{JiraTaskId}.sh
@@ -5,7 +5,7 @@ cpu_max=$((`nproc` / 2))
  
 for ((cpu_num = 1; cpu_num <= cpu_max; cpu_num++))
 do
-    cat {JiraTaskId}.sql \
+    cat {JiraTaskId}_do.sql \
         | sed "s/cpu_num constant smallint default 1/cpu_num constant smallint default $cpu_num/g" \
         | sed "s/cpu_max constant smallint default 1/cpu_max constant smallint default $cpu_max/g" \
         | psql --user={username} --dbname={dbname} --echo-all --set="ON_ERROR_STOP=1" \
