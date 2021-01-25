68c3ef8b58472b48d4d0321ba9fdb5b9cc0fdbad
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-01-25 19:52:42 +0300

    Update {JiraTaskId}_do.sh

--- modify_million_rows/{JiraTaskId}_do.sh
+++ modify_million_rows/{JiraTaskId}_do.sh
@@ -1,18 +1,22 @@
 #cpu_max=`nproc`
 cpu_max=$((`nproc` / 2))
 #echo "$cpu_max"
+
+name=`basename $0 .sh`
+
 #exit 0
  
 for ((cpu_num = 1; cpu_num <= cpu_max; cpu_num++))
 do
-    cat {JiraTaskId}_do.sql \
+    cat ${name}.sql \
         | sed "s/cpu_num constant smallint default 1/cpu_num constant smallint default $cpu_num/g" \
         | sed "s/cpu_max constant smallint default 1/cpu_max constant smallint default $cpu_max/g" \
-        | psql --user={username} --dbname={dbname} --echo-all --set="ON_ERROR_STOP=1" \
-               --log-file={JiraTaskId}_do_$cpu_num.log 2> {JiraTaskId}_do_$cpu_num.stderr.log &
+        | psql postgresql://{user}:{password}@{host}:5433/{database}?application_name=${0} \
+               --echo-all --set="ON_ERROR_STOP=1" \
+               --log-file=${name}_${cpu_num}.log 2> ${name}_${cpu_num}.stderr.log &
 done
- 
+
 jobs -l
- 
+
 #wait
 #echo "All done"
