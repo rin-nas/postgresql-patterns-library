114a1c20531a9daa2c56107c8c5db4daa890a57b
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-04-14 17:38:13 +0300

    Update {JiraTaskId}_finish.sql

--- modify_million_rows/{JiraTaskId}_finish.sql
+++ modify_million_rows/{JiraTaskId}_finish.sql
@@ -1,5 +1,5 @@
 VACUUM VERBOSE ANALYZE {table};
 
 DROP TABLE IF EXISTS {table}_{JiraTaskId};
--- или, если временная таблица является резервной копией нужно удалить её позже, то переносим в другую схему
+-- или переносим в другую схему, если временная таблица является резервной копией и нужно удалить её позже
 ALTER TABLE IF EXISTS {table}_{JiraTaskId} SET SCHEMA migration;
