264fb959eb8d37065b812e0036b2806981f7f074
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-13 20:38:16 +0300

    Update bit_get.sql

--- functions/bit_get.sql
+++ functions/bit_get.sql
@@ -17,30 +17,6 @@ begin
 end
 $$;
 
-CREATE OR REPLACE FUNCTION bit_set(num bigint, pos int, val boolean)
-    -- устанавливает для числа в заданной позиции бит в 1 или 0
-    RETURNS bigint
-    stable
-    returns null on null input
-    SECURITY INVOKER
-    PARALLEL SAFE
-    LANGUAGE plpgsql
-AS $$
-DECLARE
-    mask bigint default 1 << (pos - 1);
-begin
-    if num < 0 then
-        raise exception 'First procedure argument should be >= 0, but % given', num;
-    elsif pos not between 1 and 64 then
-        raise exception 'Second procedure argument should be between 1 and 64, but % given', pos;
-    end if;
-    if val then
-        return num | mask;
-    else
-        return num & ~mask;
-    end if;
-end
-$$;
 
 --TEST
 do $$
