9ed4f49b00653ca55d61dfcf93f127198ec98f7c
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-16 13:27:05 +0300

    Update email_parse.sql

--- functions/email_parse.sql
+++ functions/email_parse.sql
@@ -1,18 +1,26 @@
 
-create or replace function email_parse(email text)
-    -- парсит email, возвращает record из 2-х элементов
-    -- возвращает null, если строка не является email (минимальная проверка синтаксиса)
+create or replace function depers.email_parse(email text)
+    -- парсит email, возвращает record из 2-х элементов: username и domain
+    -- возвращает null, если email невалиден (минимальная проверка синтаксиса)
     returns table (username text, domain text)
     stable
     returns null on null input
     parallel safe
-    language sql
+    language plpgsql
 as
 $$
-    select t[1] as username,
-           t[2] as domain
-    from regexp_match(email, '^(.+)@([^@]+)$', '') as t
+    declare
+        -- https://en.wikipedia.org/wiki/Email_address
+        username_max_length smallint default 64;
+        domain_max_length smallint default 255;
+    begin
+        return query
+        select case when octet_length(t[1]) > username_max_length then null else t[1] end as username,
+               case when octet_length(t[2]) > domain_max_length then null else t[2] end as domain
+        from regexp_match(email, '^(.+)@([^@]+)$', '') as t;
+    end
 $$;
+
 
 -- TEST
 select * from email_parse('111@222@ya.ru');
