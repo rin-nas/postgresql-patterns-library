3181fe578771227ebc2c2ec57717c5908fd1bd8d
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-22 16:28:08 +0300

    Update quote_like.sql

--- functions/quote_like.sql
+++ functions/quote_like.sql
@@ -1,12 +1,12 @@
 create function quote_like(text) returns text
-    immutable
-    strict
+    stable
+    returns null on null input
+    parallel safe
     language sql
-    PARALLEL SAFE -- Postgres 10 or later
 as
 $$
 SELECT replace(replace(replace($1
-                               , '\', '\\') -- must come 1st
+                               , '\', '\\') -- must come first!
                                , '_', '\_')
                                , '%', '\%');
 $$;
