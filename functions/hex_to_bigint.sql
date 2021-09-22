5c6909c26c5b1ebd98dd023f07d5459ddf686b9a
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-22 18:53:04 +0300

    Create hex_to_bigint.sql

---
+++ functions/hex_to_bigint.sql
@@ -0,0 +1,19 @@
+-- Adapted by Rinat from https://stackoverflow.com/questions/8316164/convert-hex-in-text-representation-to-decimal-number/8316731
+
+create or replace function hex_to_bigint(hexval text) 
+returns bigint 
+strict 
+immutable
+language sql
+as 
+$$
+select bit_or(get_byte(
+              decode(lpad(hexval, 32, '0'), 'hex')
+             , g)::int8 << ((15 - g) * 8))
+from generate_series(0, 15) as g
+$$;
+
+--TEST
+
+-- MD5 to BigInt
+select hex_to_bigint(md5('test'));
