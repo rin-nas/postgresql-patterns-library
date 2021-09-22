4a973ddda637151d23db6221abc0d2508e8e5755
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-22 16:26:22 +0300

    Update quote_regexp.sql

--- functions/quote_regexp.sql
+++ functions/quote_regexp.sql
@@ -1,6 +1,8 @@
 -- https://stackoverflow.com/questions/5144036/escape-function-for-regular-expression-or-like-patterns/45741630#45741630
 create function quote_regexp(text) returns text
     stable
+    returns null on null input
+    parallel safe
     language plpgsql
 as
 $$
