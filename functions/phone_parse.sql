3ca72c2ec58d66307242a83099b70a6ebac331fa
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-24 19:12:20 +0300

    Update phone_parse.sql

--- functions/phone_parse.sql
+++ functions/phone_parse.sql
@@ -1,25 +1,29 @@
-create function phone_parse(phone text)
+create or replace function phone_parse(
+    phone text,
+    country_code out text,
+    area_code out text,
+    local_number out text
+)
     -- парсит номер телефона в международном формате: +<countryCode><space><areaCode><space><localNumber>
     -- возвращает null, если строка не является номером телефона (минимальная проверка синтаксиса)
-    returns table (
-        country_code text,
-        area_code text,
-        local_number text
-    )
+    returns record
     stable
     returns null on null input
     parallel safe
     language sql
 as
 $$
-    select t[1] as country_code,
-           t[2] as area_code,
-           t[3] as local_number
-    from regexp_match(phone, '^\+(\d+) (\d+) (\d+)$', '') as t
+select t[1] as country_code,
+       t[2] as area_code,
+       t[3] as local_number
+from regexp_match(phone, '^\+(\d+) (\d+) (\d+)$', '') as t
 $$;
 
 --TEST
-select t.country_code, t.area_code, t.local_number
-from phone_parse('+375 17 1234567') as t;
-
+select * from phone_parse('+375 17 1234567') as t;
 select (phone_parse('+375 17 1234567')).country_code as t;
+
+select t.country_code is not null
+       and t.area_code is not null
+       and t.local_number is not null as is_phone
+from phone_parse('+375 17 1234567') as t;
