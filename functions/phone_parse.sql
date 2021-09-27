010e2da8f7590052f9bf766df3ba54dcf262e6d2
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-27 13:19:25 +0300

    Update phone_parse.sql

--- functions/phone_parse.sql
+++ functions/phone_parse.sql
@@ -5,6 +5,7 @@ create or replace function phone_parse(
     local_number out text
 )
     -- парсит номер телефона в международном формате: +<countryCode><space><areaCode><space><localNumber>
+    -- для маленьких стран <areaCode> может отсутствовать
     -- возвращает null, если строка не является номером телефона (минимальная проверка синтаксиса)
     returns record
     stable
@@ -14,20 +15,43 @@ create or replace function phone_parse(
 as
 $$
 --https://www.cm.com/blog/how-to-format-international-telephone-numbers/
-select t[1] as country_code,
-       t[2] as area_code,
-       t[3] as local_number
-from regexp_match(phone, '^\+(\d{1,3}) (\d+) (\d+)$', '') as t
-where length(regexp_replace(phone, '\D+', '', 'g'))
-      between 7 --https://stackoverflow.com/questions/14894899/what-is-the-minimum-length-of-a-valid-international-phone-number
-      and 15 --https://en.wikipedia.org/wiki/E.164 and https://en.wikipedia.org/wiki/Telephone_numbering_plan
+with t as (
+    -- грубая проверка проверка синтаксиса и нормализация номера телефона
+    select n
+    from trim(regexp_replace(phone, '[+ ()\-.]+', ' ', 'g')) as n
+    where octet_length(phone) between (8+1/*+*/) and (15*2)
+      and phone ~ '\+\d'
+)
+select p[1] as country_code,
+       p[2] as area_code,
+       p[3] as local_number
+from t cross join regexp_match(t.n, '^
+                                      (\d{1,3})\x20   #country_code
+                                      (?:(\d+)\x20)?  #area_code
+                                      (\d+)           #local_number
+                                     $', 'x') as p
+-- проверяем кол-во цифр
+where octet_length(replace(t.n, ' ', ''))
+          between 8 --https://stackoverflow.com/questions/14894899/what-is-the-minimum-length-of-a-valid-international-phone-number
+          and 15; --https://en.wikipedia.org/wiki/E.164 and https://en.wikipedia.org/wiki/Telephone_numbering_plan
 $$;
 
---TEST
-select * from phone_parse('+375 17 1234567') as t;
-select (phone_parse('+375 17 1234567')).country_code as t;
-
-select t.country_code is not null
-       and t.area_code is not null
-       and t.local_number is not null as is_phone
-from phone_parse('+375 17 1234567') as t;
+-- TEST
+do $$
+    begin
+        --positive
+        assert (select country_code = '375'
+                           and area_code = '17'
+                           and local_number = '1234567'
+                from phone_parse('+375 17 1234567'));
+        assert (select country_code = '677'
+                           and area_code is null
+                           and local_number = '12345'
+                from phone_parse('+677 12345'));
+        assert (phone_parse('+375 17 1234567')).country_code = '375';
+        --negative
+        assert phone_parse('+375171234567') is null; --syntax
+        assert phone_parse('+677 1234') is null; --minimum length
+        assert phone_parse('+375 17 12345671234567890') is null; --maximum length
+    end
+$$;
