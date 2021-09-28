62ce2bf1699a2779539c8a1d7e2049a1bab739c7
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-28 23:30:53 +0300

    Update is_sql.sql

--- functions/is_sql.sql
+++ functions/is_sql.sql
@@ -1,5 +1,8 @@
 -- check SQL syntax exactly in your PostgreSQL version
-CREATE OR REPLACE FUNCTION is_sql(sql text) returns boolean
+CREATE OR REPLACE FUNCTION depers.is_sql(sql text, is_notice boolean default false)
+    returns boolean
+    returns null on null input
+    parallel safe
     language plpgsql
 AS
 $$
@@ -8,13 +11,15 @@ DECLARE
     exception_context text;
 BEGIN
     BEGIN
-        EXECUTE E'DO $IS_SQL$ BEGIN\nRETURN;\n' || trim(trailing E'; \r\n' from sql) || E';\nEND; $IS_SQL$;';
+        EXECUTE E'DO $IS_SQL$ BEGIN\nRETURN;\n' || trim(trailing E'; \r\n\t' from sql) || E';\nEND; $IS_SQL$;';
     EXCEPTION WHEN syntax_error THEN
         GET STACKED DIAGNOSTICS
             exception_message = MESSAGE_TEXT,
             exception_context = PG_EXCEPTION_CONTEXT;
-        RAISE NOTICE '%', exception_context;
-        RAISE NOTICE '%', exception_message;
+        IF is_notice THEN
+            RAISE NOTICE '%', exception_context;
+            RAISE NOTICE '%', exception_message;
+        END IF;
         RETURN FALSE;
     END;
     RETURN TRUE;
@@ -22,4 +27,9 @@ END
 $$;
 
 -- TEST
-SELECT is_sql('SELECTx'), is_sql('SELECT x');
+do $$
+    begin
+        assert not depers.is_sql('SELECTx', true);
+        assert depers.is_sql('SELECT x');
+    end;
+$$;
