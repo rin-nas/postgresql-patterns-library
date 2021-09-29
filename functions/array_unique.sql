202d8138d5820c48ef2fadd88736f35438e05111
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-09-29 15:49:13 +0300

    Create array_unique.sql

---
+++ functions/array_unique.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION depers.array_unique(anyarray)
+    RETURNS anyarray
+    stable
+    returns null on null input
+    parallel safe
+    language sql
+AS $$
+    SELECT array_agg(DISTINCT x ORDER BY x) FROM unnest($1) t(x);
+$$;
