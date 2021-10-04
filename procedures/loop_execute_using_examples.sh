741a17c8f0a06793c012ddb1a00bcefd31ed2015
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-10-05 00:08:24 +0300

    Update loop_execute_using_examples.sh

--- procedures/loop_execute_using_examples.sh
+++ procedures/loop_execute_using_examples.sh
@@ -1,4 +1,4 @@
-#распараллеливание выполнения loop_execute по ядрам процессора
+#распараллеливание выполнения SQL запроса по ядрам процессора
 
 #cpu_max=`nproc`
 cpu_max=$((`nproc` / 2))
@@ -12,11 +12,22 @@ database='database' #modify me!
 
 #exit 0 #ok
 #exit 1 #error
- 
+
+regexp="\buse_cpu\s*\(\s*([^,]+)\s*,\s*[0-9]+\s*,\s*[0-9]+\s*\)"
+
+if cat "${name}.sql" | grep -P -e "${regexp}"; then
+else
+    echo "Функция use_cpu() для распараллеливания SQL запроса по ${cpu_max} ядрам процессора НЕ найдена! Будет использовано только 1 ядро.
+    cpu_max=1
+    exit 0 #ok
+fi
+
+echo "Распараллеливаем SQL запрос по ${cpu_max} ядрам процессора"
+
 for ((cpu_num = 1; cpu_num <= cpu_max; cpu_num++))
 do
-    cat ${name}.sql \
-        | sed -E -e "s/\buse_cpu\s*\(\s*([^,]+)\s*,\s*[0-9]+\s*,\s*[0-9]+\s*\)/use_cpu(\1, ${cpu_num}, ${cpu_max})/g" \
+    cat "${name}.sql" \
+        | sed -E -e "s/${regexp}/use_cpu(\1, ${cpu_num}, ${cpu_max})/g" \
         | psql postgresql://${user}:${password}@${host}:5433/${database}?application_name=${0} \
                --echo-all --set="ON_ERROR_STOP=1" \
                --log-file=${name}_${cpu_num}.log 2> ${name}_${cpu_num}.stderr.log &
