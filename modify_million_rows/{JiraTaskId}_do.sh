adb39fcad474ce1fef50226cfe72f9259249f4f4
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-10-06 16:28:27 +0300

    Update {JiraTaskId}_do.sh

--- modify_million_rows/{JiraTaskId}_do.sh
+++ modify_million_rows/{JiraTaskId}_do.sh
@@ -6,9 +6,9 @@ cpu_max=$((`nproc` / 2))
 
 name=`basename $0 .sh`
 user='user'         #modify me!
-password='password' #modify me!
 host='host'         #modify me!
 database='database' #modify me!
+#пароль укажите в файле ~/.pgpass, нельзя хранить в GIT этот файл с настоящими настройками подключения к БД!
 
 #exit 0
  
@@ -17,12 +17,12 @@ do
     cat ${name}.sql \
         | sed "s/cpu_num constant smallint default 1/cpu_num constant smallint default $cpu_num/g" \
         | sed "s/cpu_max constant smallint default 1/cpu_max constant smallint default $cpu_max/g" \
-        | psql postgresql://${user}:${password}@${host}:5433/${database}?application_name=${0} \
+        | psql postgresql://${user}@${host}:5433/${database}?application_name=${0} \
                --echo-all --set="ON_ERROR_STOP=1" \
                --log-file=${name}_${cpu_num}.log 2> ${name}_${cpu_num}.stderr.log &
 done
 
 jobs -l
 
-#wait
+#wait #wait for all remaining workers
 #echo "All done"
