c1fbd0f67913433b48aceee3dc35774677cb4391
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-10-06 16:44:44 +0300

    Update loop_execute_using_examples.sh

--- procedures/loop_execute_using_examples.sh
+++ procedures/loop_execute_using_examples.sh
@@ -5,10 +5,10 @@ cpu_max=$((`nproc` / 2))
 #echo "$cpu_max"
 
 name=`basename $0 .sh`
-user='user'         #modify me!
-password='password' #modify me!
 host='host'         #modify me!
 database='database' #modify me!
+user='user'         #modify me!
+#пароль укажите в файле ~/.pgpass
 
 #exit 0 #ok
 #exit 1 #error
@@ -28,8 +28,9 @@ for ((cpu_num = 1; cpu_num <= cpu_max; cpu_num++))
 do
     cat "${name}.sql" \
         | sed -E -e "s/${regexp}/use_cpu(\1, ${cpu_num}, ${cpu_max})/g" \
-        | psql postgresql://${user}:${password}@${host}:5433/${database}?application_name=${0} \
-               --echo-all --set="ON_ERROR_STOP=1" \
+        | psql postgresql://${user}@${host}:5433/${database}?application_name=${0} \
+               --echo-all \
+               --set="ON_ERROR_STOP=1" \
                --log-file=${name}_${cpu_num}.log 2> ${name}_${cpu_num}.stderr.log &
 done
 
