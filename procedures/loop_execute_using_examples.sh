85b40d84bd8d2cab1e97e21916b7854cfa6d93b7
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-10-08 02:49:47 +0300

    Update loop_execute_using_examples.sh

--- procedures/loop_execute_using_examples.sh
+++ procedures/loop_execute_using_examples.sh
@@ -1,9 +1,9 @@
 #распараллеливание выполнения SQL запроса по ядрам процессора
 #для миграций длительностью < 5 минут этот скрипт не нужен, можно подождать
 
-#cpu_max=`nproc`
-cpu_max=$((`nproc` / 2))
-#echo "$cpu_max"
+#core_max=`nproc`
+core_max=$((`nproc` / 2))
+#echo "$core_max"
 
 name=`basename $0 .sh`
 host='host'         #modify me!
@@ -14,26 +14,26 @@ user='user'         #modify me!
 #exit 0 #ok
 #exit 1 #error
 
-regexp="\buse_cpu\s*\(\s*([^,]+)\s*,\s*[0-9]+\s*,\s*[0-9]+\s*\)"
+regexp="\bcpu_core\s*\(\s*([^,]+)\s*,\s*[0-9]+\s*,\s*[0-9]+\s*\)"
 
 if cat "${name}.sql" | grep -q -P -e "${regexp}"; then
-    echo "В SQL запросе есть вызов функции use_cpu()."
-    echo "Распараллеливаем SQL запрос по ${cpu_max} ядрам процессора"
+    echo "В SQL запросе есть вызов функции cpu_core()."
+    echo "Распараллеливаем SQL запрос по ${core_max} ядрам процессора"
 else
-    echo "В SQL запросе нет вызова функции use_cpu()."
-    echo "Поэтому распараллеливания выполнения по ${cpu_max} ядрам процессора не будет!"
+    echo "В SQL запросе нет вызова функции cpu_core()."
+    echo "Поэтому распараллеливания выполнения по ${core_max} ядрам процессора не будет!"
     echo "Будет использовано только 1 ядро."
-    cpu_max=1
+    core_max=1
 fi
 
-for ((cpu_num = 1; cpu_num <= cpu_max; cpu_num++))
+for ((core_num = 1; core_num <= core_max; core_num++))
 do
     cat "${name}.sql" \
-        | sed -E -e "s/${regexp}/use_cpu(\1, ${cpu_num}, ${cpu_max})/g" \
+        | sed -E -e "s/${regexp}/cpu_core(\1, ${core_num}, ${core_max})/g" \
         | psql postgresql://${user}@${host}:5433/${database}?application_name=${0} \
                --echo-all \
                --set="ON_ERROR_STOP=1" \
-               --log-file=${name}_${cpu_num}.log 2> ${name}_${cpu_num}.stderr.log &
+               --log-file=${name}_${core_num}.log 2> ${name}_${core_num}.stderr.log &
 done
 
 jobs -l
