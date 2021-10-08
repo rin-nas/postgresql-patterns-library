2961d6e668ed0ef5a5e9b4a9b017658aa65f4f27
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-10-08 03:01:54 +0300

    Update and rename cpu_core.sql to use_parallel.sql

--- functions/use_parallel.sql
+++ functions/use_parallel.sql
@@ -1,11 +1,11 @@
-create or replace function cpu_core(
+create or replace function use_parallel(
     id bigint,
     core_num integer,
     core_max integer
 )
     -- Функция предназначена для распараллеливания SQL запросов
     -- Принимает решение, использовать преданный номер ядра процессора или нет
-    -- Пример: WHERE cpu_core(id, 1, 5)
+    -- Пример: WHERE use_parallel(id, 1, 5)
     returns boolean
     stable
     returns null on null input
@@ -28,20 +28,20 @@ $$;
 
 --TEST
 select
-    sum(cpu_core(g, 1, 3)::int), --3333
-    sum(cpu_core(g, 2, 3)::int), --3334
-    sum(cpu_core(g, 3, 3)::int) --3333
+    sum(use_parallel(g, 1, 3)::int), --3333
+    sum(use_parallel(g, 2, 3)::int), --3334
+    sum(use_parallel(g, 3, 3)::int) --3333
 from generate_series(1, 10000) as g;
 
 ------------------------------------------------------------------------------------------------------------------------
-create or replace function cpu_core(
+create or replace function use_parallel(
     str text,
     core_num integer,
     core_max integer
 )
     -- Функция предназначена для распараллеливания SQL запросов
     -- Принимает решение, использовать преданный номер процессора или нет
-    -- Пример: WHERE cpu_core('mike@domain.com', 1, 5)
+    -- Пример: WHERE use_parallel('mike@domain.com', 1, 5)
     returns boolean
     stable
     returns null on null input
@@ -58,14 +58,13 @@ BEGIN
         RAISE EXCEPTION 'Argument core_num must be <= core_max! Given core_num = %, core_max = %', core_num, core_max;
     END IF;
 
-    -- https://github.com/rin-nas/postgresql-patterns-library/blob/master/functions/crc32.sql
     RETURN abs(crc32(str)) % core_max = core_num - 1;
 END;
 $$;
 
 --TEST
 select
-    sum(cpu_core(g::text, 1, 3)::int), --3307
-    sum(cpu_core(g::text, 2, 3)::int), --3358
-    sum(cpu_core(g::text, 3, 3)::int) --3335
+    sum(use_parallel(g::text, 1, 3)::int), --3307
+    sum(use_parallel(g::text, 2, 3)::int), --3358
+    sum(use_parallel(g::text, 3, 3)::int) --3335
 from generate_series(1, 10000) as g;
