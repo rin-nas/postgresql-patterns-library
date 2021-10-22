07abc3992c9b1ebae1e285d94a93b72b1026187b
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-10-22 03:01:23 +0300

    Create is_regexp.sql

---
+++ functions/is_regexp.sql
@@ -0,0 +1,41 @@
+-- check Regexp syntax exactly in your PostgreSQL version
+CREATE OR REPLACE FUNCTION is_regexp(regexp text, is_notice boolean default false)
+    returns boolean
+    returns null on null input
+    parallel safe
+    language plpgsql
+AS
+$$
+DECLARE
+    exception_message text;
+    exception_context text;
+BEGIN
+    BEGIN
+        PERFORM '' ~ regexp;
+    EXCEPTION WHEN invalid_regular_expression THEN
+        GET STACKED DIAGNOSTICS
+            exception_message = MESSAGE_TEXT,
+            exception_context = PG_EXCEPTION_CONTEXT;
+        IF is_notice THEN
+            RAISE NOTICE '%', exception_context;
+            RAISE NOTICE '%', exception_message;
+        END IF;
+        RETURN FALSE;
+    END;
+    RETURN TRUE;
+END
+$$;
+
+-- TEST
+do $$
+    begin
+        --positive
+        assert is_regexp('^[z]+\d$');
+        assert is_regexp('');
+
+        --negative
+        assert not is_regexp('[', true);
+        assert not is_regexp('*', true);
+        assert not is_regexp('\', true);
+    end;
+$$;
