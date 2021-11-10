d7dc362fb1242da34fa9efbc17fc7f61796b65a1
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-11-10 16:45:08 +0300

    Create is_inet.sql

---
+++ functions/is_inet.sql
@@ -0,0 +1,44 @@
+-- check IP syntax exactly in your PostgreSQL version
+create or replace function is_inet(str text, is_notice boolean default false)
+    RETURNS boolean
+    returns null on null input
+    parallel safe
+    stable
+    language plpgsql
+as
+$$
+DECLARE
+    exception_sqlstate text;
+    exception_message text;
+    exception_context text;
+BEGIN
+    BEGIN
+        RETURN (str::inet is not null);
+    EXCEPTION WHEN others THEN
+        GET STACKED DIAGNOSTICS
+            exception_sqlstate = RETURNED_SQLSTATE,
+            exception_message = MESSAGE_TEXT,
+            exception_context = PG_EXCEPTION_CONTEXT;
+        IF is_notice THEN
+            RAISE NOTICE 'exception_sqlstate = %', exception_sqlstate;
+            RAISE NOTICE 'exception_context = %', exception_context;
+            RAISE NOTICE 'exception_message = %', exception_message;
+        END IF;
+        RETURN FALSE;
+    END;
+END;
+$$;
+
+--TEST
+do $$
+    begin
+        --positive
+        assert is_inet('0.0.0.0');
+        assert is_inet('255.255.255.255');
+        assert is_inet('192.168.0.1/24');
+        --negative
+        assert not is_inet('0.0.0');
+        assert not is_inet('255.255.255.256');
+        assert not is_inet('192.168.0.1/244');
+    end
+$$;
