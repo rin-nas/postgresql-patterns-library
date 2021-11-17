5fd2af6f088fd1870e5b8ec10a4f6896b3af5d43
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-11-18 01:42:39 +0300

    Update execute_attempt.sql

--- procedures/execute_attempt.sql
+++ procedures/execute_attempt.sql
@@ -5,40 +5,42 @@ create or replace procedure execute_attempt(
     query text,
     --необязательные параметры:
     lock_timeout text default '100ms',
-    max_attempts smallint default 50
+    max_attempts int default 50
 )
     language plpgsql
 as
 $procedure$
 declare
-    lock_timeout_old text default current_setting('lock_timeout');
-    is_completed boolean default false;
-    delay numeric;
-    total_time_start timestamp not null default clock_timestamp();
-    total_time_elapsed numeric not null default 0; -- длительность выполнения всех запросов, в секундах
+    lock_timeout_old constant text not null default current_setting('lock_timeout');
+    time_start constant timestamp not null default clock_timestamp();
+    time_elapsed numeric not null default 0; -- длительность выполнения всех запросов, в секундах
+    delay numeric not null default 0;
 begin
     perform set_config('lock_timeout', lock_timeout, true);
 
-    for i in 1..max_attempts loop
+    for cur_attempt in 1..max_attempts loop
         begin
             execute query;
-            is_completed := true;
+            perform set_config('lock_timeout', lock_timeout_old, true);
             exit;
         exception when lock_not_available then
-            total_time_elapsed := round(extract('epoch' from clock_timestamp() - total_time_start)::numeric, 2);
-            delay := round(greatest(sqrt(total_time_elapsed * 1), 1), 2);
-            raise warning 'Attempt % of % to execute query failed due lock timeout %, next replay after % second', i, max_attempts, lock_timeout, delay;
-            perform pg_sleep(delay);
+            if cur_attempt < max_attempts then
+                time_elapsed := round(extract('epoch' from clock_timestamp() - time_start)::numeric, 2);
+                delay := round(greatest(sqrt(time_elapsed * 1), 1), 2);
+                raise warning
+                    'Attempt % of % to execute query failed due lock timeout %, next replay after % second',
+                    cur_attempt, max_attempts, lock_timeout, delay;
+                perform pg_sleep(delay);
+            else
+                perform set_config('lock_timeout', lock_timeout_old, true);
+                raise warning
+                    'Attempt % of % to execute query failed due lock timeout %',
+                    cur_attempt, max_attempts, lock_timeout;
+                raise; -- raise the original exception
+            end if;
         end;
     end loop;
 
-    perform set_config('lock_timeout', lock_timeout_old, true);
-
-    if is_completed then
-        raise info 'Execute success';
-    else
-        raise exception 'Execute failed';
-    end if;
 end
 $procedure$;
 
@@ -47,12 +49,9 @@ comment on procedure execute_attempt(
     query text,
     --необязательные параметры:
     lock_timeout text,
-    max_attempts smallint
+    max_attempts int
 ) is $$
     Процедура предназначена для безопасного выполнения DDL запросов в БД. Например, миграций БД.
     Пытается выполнить запрос с учётом ограничения lock_timeout.
     В случае неудачи делает задержку выполнения и повторяет попытку N раз.
 $$;
-
---TEST
---call depers.execute_attempt('alter table person alter column email type varchar(320)');
