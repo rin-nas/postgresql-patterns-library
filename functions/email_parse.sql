822db5a9a613e1be7a9aac509fa18b1e95c7bcaa
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-12-12 23:54:07 +0300

    Update email_parse.sql

--- functions/email_parse.sql
+++ functions/email_parse.sql
@@ -6,7 +6,7 @@ create or replace function email_parse(
     -- парсит email, возвращает record из 2-х элементов: username и domain
     -- возвращает null, если email невалиден (минимальная проверка синтаксиса)
     returns record
-    stable
+    immutable
     returns null on null input
     parallel safe
     language sql
@@ -16,17 +16,16 @@ $$
     select t[1] as username, t[2] as domain
     from regexp_match(email, '^(.+)@([^@]+)$', '') as t
     where octet_length(email) between 6 and 320
-          and octet_length(t[1]) <= 64 and octet_length(t[2]) <= 255;
+      and position('@' in email) > 1 --speed improves
+      and octet_length(t[1]) <= 64 and octet_length(t[2]) <= 255;
 $$;
 
 -- TEST
 do $$
     begin
         --positive
-        assert (select username = '111@222'
-                       and domain = 'ya.ru'
+        assert (select username = '111@222' and domain = 'ya.ru'
                 from email_parse('111@222@ya.ru') as t);
-        assert (email_parse('111@222@ya.ru')).domain = 'ya.ru';
         --negative
         assert email_parse('123@') is null;
         assert email_parse('@123') is null;
