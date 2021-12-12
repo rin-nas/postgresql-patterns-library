c894e2c4de0d2e2a2594bc4768d2ac247275e676
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-12-13 01:15:41 +0300

    Create app_progress.sql

---
+++ functions/app_progress.sql
@@ -0,0 +1,37 @@
+create or replace function app_progress(
+    done_percent numeric, --прогресс выполнения некого запроса
+    prefix char default '#', --символ-маркер, после которого должно быть число процентов
+    is_local bool default false --false allow pass application_name's value to subtransaction
+)
+    returns text
+    returns null on null input
+    parallel unsafe --NOT safe!
+    volatile --NOT stable!
+    language plpgsql
+as
+$$
+declare
+    app_name text;
+begin
+    app_name := current_setting('application_name');
+    app_name := regexp_replace(app_name, concat('\s*\', prefix, '\d+(?:\.\d+)?%'), '');
+    app_name := concat(app_name, ' ', prefix, done_percent, '%');
+    if octet_length(app_name) > 63 then
+        return app_name;
+    end if;
+    return set_config('application_name', app_name, is_local);
+end
+$$;
+
+
+comment on function app_progress(
+    done_percent numeric,
+    prefix char,
+    is_local bool
+) is $$
+    Дописывает или заменяет прогресс выполнения (в процентах) "тяжёлого" запроса в application_name.
+    Т.о. его можно наблюдать в списке процессов БД и отслеживать ход выполнения.
+$$;
+
+--TEST
+--select app_progress(0), current_setting('application_name'), app_progress(1), current_setting('application_name');
