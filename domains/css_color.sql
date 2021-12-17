edfed5be3dc9fa580f140d43afc028fbbc38a65a
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2021-12-17 14:12:19 +0300

    Create css_color.sql

---
+++ domains/css_color.sql
@@ -0,0 +1,11 @@
+-- https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
+-- https://regex101.com/r/CMQKwv/3/
+
+CREATE DOMAIN css_color AS text CHECK(
+    octet_length(VALUE) BETWEEN 4 AND 9 
+    AND VALUE ~ '^#[a-fA-F\d]{3}(?:[a-fA-F\d]{3})?$|^#[a-fA-F\d]{4}(?:[a-fA-F\d]{4})?$'
+);
+
+--TEST
+select '#777'::css_color; --ok
+select '$777'::css_color; --error
