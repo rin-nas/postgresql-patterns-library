709fa83f1882ff10fd0d587c0a37d4bc4ec267b9
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2022-04-22 17:44:06 +0300

    Update replace_pairs.sql

--- functions/replace_pairs.sql
+++ functions/replace_pairs.sql
@@ -6,6 +6,7 @@ create or replace function replace_pairs(str text, input jsonb)
     returns null on null input
     parallel safe -- postgres 10 or later
     language plpgsql
+    cost 3
 as
 $func$
 declare
@@ -25,6 +26,40 @@ $func$;
 do $$
 begin
     assert replace_pairs('aaabaaba', jsonb_build_object(
+        'aa', 2,
+        'a', 1
+    )) = '21b2b1';
+end
+$$;
+
+------------------------------------------------------------------------------------------------------------------------
+
+create or replace function replace_pairs(str text, input json)
+    returns text
+    immutable
+    returns null on null input
+    parallel safe -- postgres 10 or later
+    language plpgsql
+    cost 3
+as
+$func$
+declare
+    rec record;
+begin
+    for rec in
+        select * from json_each_text(input)
+    loop
+        str := replace(str, rec.key, rec.value);
+    end loop;
+
+    return str;
+end
+$func$;
+
+-- TEST
+do $$
+begin
+    assert replace_pairs('aaabaaba', json_build_object(
         'aa', 2,
         'a', 1
     )) = '21b2b1';
