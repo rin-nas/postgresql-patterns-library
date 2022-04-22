833bcaeee5cd64aa4f78bc454391dfedea79fb10
Rinat Mukhatrov <rin-nas@users.noreply.github.com>
2022-04-22 17:46:29 +0300

    Update is_date.sql

--- functions/is_date.sql
+++ functions/is_date.sql
@@ -4,6 +4,8 @@ create or replace function is_date(str text, is_notice boolean default false)
     --parallel safe
     stable
     language plpgsql
+    set datestyle = 'ISO, DMY'
+    cost 5
 as
 $$
 DECLARE
@@ -11,16 +13,19 @@ DECLARE
     exception_message text;
     exception_context text;
 BEGIN
+    -- сначала проверяем формат ввода (YYYYMMDD не поддерживается!)
+    if not (
+        octet_length(str) = 10 --speed improves
+        and (str ~ '^\d{4}-\d{2}-\d{2}$' /*YYYY-MM-DD*/ or str ~ '^\d{2}\.\d{2}\.\d{4}$'/*DD.MM.YYYY*/)
+       )
+    then
+        return false;
+    end if;
+
     BEGIN
-        -- сначала проверяем формат ввода (YYYYMMDD не поддерживается!)
-        if octet_length(str) = 10 --speed improves
-           and (str ~ '^\d{4}-\d{2}-\d{2}$' /*YYYY-MM-DD*/ or str ~ '^\d{2}\.\d{2}\.\d{4}$'/*DD.MM.YYYY*/)
-        then
-            -- теперь проверяем наличие несущестующей даты
-            -- make_date() для невалидной даты тоже кидает исключение, например: [22008] ERROR: date field value out of range: 2021-02-29
-            RETURN (str::date is not null);
-        end if;
-        RETURN FALSE;
+        -- теперь проверяем наличие несущестующей даты
+        -- make_date() для невалидной даты тоже кидает исключение, например: [22008] ERROR: date field value out of range: 2021-02-29
+        RETURN (str::date is not null);
     EXCEPTION WHEN others THEN
         GET STACKED DIAGNOSTICS
             exception_sqlstate = RETURNED_SQLSTATE,
