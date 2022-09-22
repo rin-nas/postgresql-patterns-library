0da79bcfec9a6a6975595226dceed65337fd7878
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-09-22 13:07:40 +0300

    flag to drop nulls

--- functions/array_unique.sql
+++ functions/array_unique.sql
@@ -1,3 +1,4 @@
+--simplest and faster
 CREATE FUNCTION array_unique(anyarray)
     RETURNS anyarray
     stable
@@ -5,5 +6,21 @@ CREATE FUNCTION array_unique(anyarray)
     parallel safe
     language sql
 AS $$
-    SELECT array_agg(DISTINCT x ORDER BY x) FROM unnest($1) t(x);
+    SELECT array_agg(DISTINCT x) --using DISTINCT implicitly sorts the array
+    FROM unnest($1) t(x);
+$$;
+
+CREATE FUNCTION array_unique(
+      anyarray, -- input array 
+      boolean -- flag to drop nulls
+) 
+    RETURNS anyarray
+    stable
+    returns null on null input
+    parallel safe
+    language sql
+AS $$
+      SELECT array_agg(DISTINCT x) --using DISTINCT implicitly sorts the array
+      FROM unnest($1) t(x) 
+      WHERE CASE WHEN $2 THEN x IS NOT NULL ELSE true END;
 $$;
