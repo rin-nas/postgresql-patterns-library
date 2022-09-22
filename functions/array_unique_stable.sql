5f8960245d84c828e58f92f4b382933cb34bcfb0
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-09-22 13:09:43 +0300

    Create array_unique_stable.sql

---
+++ functions/array_unique_stable.sql
@@ -0,0 +1,22 @@
+/*
+Using DISTINCT implicitly sorts the array. 
+If the relative order of the array elements needs to be preserved while removing duplicates, 
+the function can be designed like the following: (should work from 9.4 onwards)
+*/
+CREATE OR REPLACE FUNCTION array_unique_stable(anyarray)
+RETURNS anyarray 
+AS $body$
+SELECT
+    array_agg(distinct_value ORDER BY first_index)
+FROM 
+    (SELECT
+        value AS distinct_value, 
+        min(index) AS first_index 
+    FROM 
+        unnest($1) WITH ORDINALITY AS input(value, index)
+    GROUP BY
+        value
+    ) AS unique_input
+;
+$body$
+LANGUAGE 'sql' IMMUTABLE STRICT;
