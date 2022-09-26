cd9ff920ba93369559595372823fbeba54094939
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-09-26 13:49:17 +0300

    Update is_payment_card.sql

--- functions/is_payment_card.sql
+++ functions/is_payment_card.sql
@@ -27,7 +27,20 @@ CREATE DOMAIN cc_number AS smallint[] CHECK ( is_valid_cc(VALUE) );
   CREATE DOMAIN cc_number AS text CHECK (
     is_valid_cc(string_to_array(VALUE, null)::smallint[])
   );
+*/
 
+/*
+--http://www.regular-expressions.info/creditcard.html
+(?=\b \d{13,16} \b)
+(?:  4[0-9]{12}(?:[0-9]{3})?          # Visa
+  |  (?: 5[1-5][0-9]{2}               # MasterCard
+       | 222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][0-9]|2720
+     ) [0-9]{12}
+  |  3[47][0-9]{13}                   # American Express
+  |  3(?:0[0-5]|[68][0-9])[0-9]{11}   # Diners Club
+  |  6(?:011|5[0-9]{2})[0-9]{12}      # Discover
+  |  (?:2131|1800|35\d{3})\d{11}      # JCB
+)
 */
 
 --TEST
