8eea9c53ae565061e320714af8c9b3a18685b2fa
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-09-26 12:00:38 +0300

    Create is_credit_card.sql

---
+++ functions/is_credit_card.sql
@@ -0,0 +1,31 @@
+--https://en.wikipedia.org/wiki/Payment_card_number
+--Most credit cards use the Luhn algorithm to validate their numbers. It is a simple checksum that helps detect single digit typos and adjacent digit transposition errors. 
+
+CREATE FUNCTION is_credit_card(smallint[]) RETURNS boolean AS $$
+  SELECT SUM(
+    CASE WHEN (pos % 2 = 0) THEN
+      2*digit - (CASE WHEN digit < 5 THEN 0 ELSE 9 END)
+    ELSE
+      digit
+    END
+  ) % 10 = 0
+  FROM
+    unnest(ARRAY( -- loop over digit/position
+      SELECT $1[i] -- ... which we read backward
+      FROM generate_subscripts($1,1) AS s(i)
+      ORDER BY i DESC
+    )
+  ) WITH ordinality AS t (digit, pos)
+$$
+LANGUAGE SQL
+IMMUTABLE;
+
+CREATE DOMAIN cc_number AS smallint[] CHECK ( is_valid_cc(VALUE) );
+
+/* alternately, store as text for user friendliness
+
+  CREATE DOMAIN cc_number AS text CHECK (
+    is_valid_cc(string_to_array(VALUE, null)::smallint[])
+  );
+
+*/
