371c4c9c48331b3ea8566a6d15b64cb90e7efc51
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-09-29 14:18:29 +0300

    Update is_ogrnip.sql

--- functions/is_ogrnip.sql
+++ functions/is_ogrnip.sql
@@ -9,7 +9,9 @@ as
 $$
     --https://ru.wikipedia.org/wiki/Основной_государственный_регистрационный_номер_индивидуального_предпринимателя
     --http://www.consultant.ru/cons/cgi/online.cgi?req=doc;base=LAW;n=179683
-    select octet_length(str) = 15 and left((left(str, 14)::bigint % 13)::text, 1) = right(str, 1)
+    select  octet_length(str) = 15
+            and str !~ '\D'
+            and left((left(str, 14)::bigint % 13)::text, 1) = right(str, 1)
 $$;
 
 comment on function is_ogrnip(text) is 'Проверяет, что переданная строка является ОГРНИП (основной государственный регистрационный номер индивидуального предпринимателя)';
@@ -26,6 +28,7 @@ BEGIN
     --negative
     assert not is_ogrnip('12345678901234');
     assert not is_ogrnip('1234567890123456');
+    assert not is_ogrnip('30776032410001a');
     assert not is_ogrnip('307760324100010');
     assert not is_ogrnip('307760324100011');
     assert not is_ogrnip('307760324100012');
