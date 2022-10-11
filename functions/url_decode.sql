8b662596e82ce6af01e4aeb68104f269a8578c41
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-10-12 02:20:58 +0300

    Create url_decode.sql

---
+++ functions/url_decode.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION url_decode(input text) RETURNS text
+LANGUAGE plpgsql IMMUTABLE STRICT AS $$
+DECLARE
+ bin bytea = '';
+ byte text;
+BEGIN
+ FOR byte IN (select (regexp_matches(input, '(%..|.)', 'g'))[1]) LOOP
+   IF length(byte) = 3 THEN
+     bin = bin || decode(substring(byte, 2, 2), 'hex');
+   ELSE
+     bin = bin || byte::bytea;
+   END IF;
+ END LOOP;
+ RETURN convert_from(bin, 'utf8');
+END
+$$;
+
+--TEST
+--select url_decode('Hell%C3%B6%20World%21') = 'Hellö World!'
