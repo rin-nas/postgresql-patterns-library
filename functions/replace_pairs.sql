dfeb4524a0d3e3a14381702beaa278fcaec34931
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-10-23 02:27:59 +0300

    depers.replace_pairs(text, jsonb) deleted

--- functions/replace_pairs.sql
+++ functions/replace_pairs.sql
@@ -1,46 +1,9 @@
 -- TODO https://wiki.postgresql.org/wiki/Multi_Replace_plpgsql
 
-create or replace function replace_pairs(str text, input jsonb)
-    returns text
-    immutable
-    returns null on null input
-    parallel safe -- postgres 10 or later
-    language plpgsql
-    cost 3
-as
-$func$
-declare
-    rec record;
-begin
-    for rec in
-        select *
-        from jsonb_each_text(input)
-        order by length(key) desc --SORT, because JSONB type reorder pairs positions!
-    loop
-        str := replace(str, rec.key, rec.value);
-    end loop;
-
-    return str;
-end
-$func$;
-
--- TEST
-do $$
-begin
-    assert replace_pairs('aaabaaba', jsonb_build_object(
-        'aa', 2,
-        'a', 1
-    )) = '21b2b1';
-    assert replace_pairs('aaabaaba', jsonb_build_object(
-        'a', 1,
-        'aa', 2
-    )) = '21b2b1';
-end
-$$;
-
-------------------------------------------------------------------------------------------------------------------------
-
-create or replace function replace_pairs(str text, input json)
+create or replace function replace_pairs(
+    str text,
+    input json --don't use jsonb type, because it reorder pairs positions!
+)
     returns text
     immutable
     returns null on null input
@@ -55,7 +18,7 @@ begin
     for rec in
         select *
         from json_each_text(input)
-        --order by length(key) desc --DO NOT SORT, because JSON type don't reorder pairs positions and we need preserve its!
+        --order by length(key) desc --DO NOT SORT, we need preserve pairs positions!
     loop
         str := replace(str, rec.key, rec.value);
     end loop;
