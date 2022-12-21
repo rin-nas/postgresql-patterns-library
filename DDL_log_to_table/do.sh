8f0975b5e1e176bcd3ec4bc79f36827609ed6b4e
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2022-12-21 12:28:36 +0300

    Update do.sh

--- DDL_log_to_table/do.sh
+++ DDL_log_to_table/do.sh
@@ -1,7 +1,4 @@
-cat DDL_log_to_table_1_table.do.sql \
-    DDL_log_to_table_2_functions.do.sql \
-    DDL_log_to_table_3_triggers.do.sql \
-    DDL_log_to_table_4_views.do.sql \
+cat $(ls -v *.do.sql) \
     | psql --username=postgres \
            --echo-errors \
            --set="ON_ERROR_STOP=1" \
