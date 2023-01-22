7c9bf98fd81e31709db738518b16658241f6101b
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-01-23 01:27:19 +0300

    returns boolean

--- functions/raise_exception.sql
+++ functions/raise_exception.sql
@@ -7,7 +7,7 @@ create or replace function raise_exception(
     hint text default 'See value in detail as JSON',
     errcode text default 'raise_exception'
 )
-    returns anyelement
+    returns boolean
     immutable
     returns null on null input
     parallel safe
@@ -38,7 +38,7 @@ select i
 from generate_series(1, 3000000) as x(i)
 where case when clock_timestamp() - statement_timestamp() < '1s'
            then true
-           else raise_exception(i)::bool
+           else raise_exception(i)
       end
 order by i;
 */
