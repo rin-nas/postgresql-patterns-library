679e82c3d910c6d097ffa200bc9f78b99a66cb3a
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-02-01 01:48:18 +0300

    test added

--- functions/raise_exception.sql
+++ functions/raise_exception.sql
@@ -1,6 +1,9 @@
 --Inspired by https://hakibenita.com/future-proof-sql
 
+--Documentation: https://postgrespro.ru/docs/postgresql/14/plpgsql-errors-and-messages
+
 --TODO: добавить ещё параметры, см. https://postgrespro.ru/docs/postgresql/14/plpgsql-errors-and-messages
+
 create or replace function raise_exception(
     value anyelement,
     message text default 'Unhandled value',
@@ -26,12 +29,51 @@ end;
 $$;
 
 --TEST
---select raise_exception(null::int);
---select raise_exception(1234567890);
---select raise_exception('ABCDE'::text);
---select raise_exception(json_build_object('id', 123));
---select raise_exception('1d2h3m4s'::interval);
---select raise_exception(now());
+do $$
+    DECLARE
+        i int not null default 0;
+
+        exception_sqlstate text;
+        exception_message text;
+        exception_context text;
+        exception_detail text;
+        exception_hint text;
+    BEGIN
+        LOOP
+            BEGIN -- subtransaction SAVEPOINT
+                i := i + 1;
+                if i = 1 then
+                    perform raise_exception(null::int);
+                elsif i = 2 then
+                    perform raise_exception(1234567890, null);
+                elsif i = 3 then
+                    perform raise_exception('ABCDE'::text, null, null);
+                elsif i = 4 then
+                    perform raise_exception(json_build_object('id', 123), null, null, null);
+                elsif i = 5 then
+                    perform raise_exception('1d2h3m4s'::interval, null, null, null, null);
+                elsif i = 6 then
+                    perform raise_exception(now());
+                end if;
+                EXIT WHEN true;
+            EXCEPTION WHEN others THEN
+                GET STACKED DIAGNOSTICS
+                    exception_sqlstate := RETURNED_SQLSTATE,
+                    exception_message  := MESSAGE_TEXT,
+                    exception_context  := PG_EXCEPTION_CONTEXT,
+                    exception_detail   := PG_EXCEPTION_DETAIL,
+                    exception_hint     := PG_EXCEPTION_HINT;
+
+                RAISE NOTICE '====== % ======', i;
+                RAISE NOTICE '* exception_sqlstate = %', exception_sqlstate;
+                RAISE NOTICE '* exception_message = %', exception_message;
+                RAISE NOTICE '* exception_context = %', exception_context;
+                RAISE NOTICE '* exception_detail = %', exception_detail;
+                RAISE NOTICE '* exception_hint = %', exception_hint;
+            END;
+        END LOOP;
+    END;
+$$;
 
 /*
 --explain
