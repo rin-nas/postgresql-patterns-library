def177756ddee01956c45c6fe8efa0b367082a74
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-02-01 01:11:16 +0300

    null support

--- functions/raise_exception.sql
+++ functions/raise_exception.sql
@@ -1,17 +1,16 @@
 --Inspired by https://hakibenita.com/future-proof-sql
 
 --TODO: добавить ещё параметры, см. https://postgrespro.ru/docs/postgresql/14/plpgsql-errors-and-messages
-
 create or replace function raise_exception(
     value anyelement,
     message text default 'Unhandled value',
-    detail text default '',
+    detail text default null,
     hint text default 'See value in detail as JSON',
     errcode text default 'raise_exception'
 )
     returns boolean
     immutable
-    returns null on null input
+    --strict -- returns null if any parameter is null
     parallel safe
     language plpgsql
     set search_path = ''
@@ -19,22 +18,20 @@ as
 $$
 begin
     raise exception using
-        message = message,
-        detail = case when detail = ''
-                      then to_json(value)::text
-                      else detail
-                 end,
-        hint = hint,
-        errcode = errcode;
+        message = coalesce(message, 'Unhandled value'),
+        detail  = coalesce(detail, coalesce(to_json(value), 'null'::json)::text),
+        hint    = coalesce(hint, 'See value in detail as JSON'),
+        errcode = coalesce(errcode, 'raise_exception');
 end;
 $$;
 
 --TEST
-
+--select raise_exception(null::int);
 --select raise_exception(1234567890);
 --select raise_exception('ABCDE'::text);
 --select raise_exception(json_build_object('id', 123));
 --select raise_exception('1d2h3m4s'::interval);
+--select raise_exception(now());
 
 /*
 --explain
