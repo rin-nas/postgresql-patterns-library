877eb79faf1eab5244e9cf1c560284b38d5fa90b
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-02-01 02:18:50 +0300

    params added

--- functions/raise_exception.sql
+++ functions/raise_exception.sql
@@ -2,14 +2,16 @@
 
 --Documentation: https://postgrespro.ru/docs/postgresql/14/plpgsql-errors-and-messages
 
---TODO: добавить ещё параметры, см. https://postgrespro.ru/docs/postgresql/14/plpgsql-errors-and-messages
-
 create or replace function raise_exception(
     value anyelement,
     message text default 'Unhandled value',
-    detail text default null,
-    hint text default 'See value in detail as JSON',
-    errcode text default 'raise_exception'
+    detail  text default null,
+    hint    text default 'See value in detail as JSON',
+    errcode text default 'raise_exception',
+    "column"     text default null,
+    "constraint" text default null,
+    "table"      text default null,
+    "schema"     text default null
 )
     returns boolean
     immutable
@@ -24,7 +26,12 @@ begin
         message = coalesce(message, 'Unhandled value'),
         detail  = coalesce(detail, coalesce(to_json(value), 'null'::json)::text),
         hint    = coalesce(hint, 'See value in detail as JSON'),
-        errcode = coalesce(errcode, 'raise_exception');
+        errcode = coalesce(errcode, 'raise_exception'),
+        column      = coalesce("column", ''),
+        constraint  = coalesce("constraint", ''),
+        table       = coalesce("table", ''),
+        schema      = coalesce("schema", ''),
+        datatype    = pg_typeof(value);
 end;
 $$;
 
@@ -38,6 +45,7 @@ do $$
         exception_context text;
         exception_detail text;
         exception_hint text;
+        exception_datatype text;
     BEGIN
         LOOP
             BEGIN -- subtransaction SAVEPOINT
@@ -53,20 +61,23 @@ do $$
                 elsif i = 5 then
                     perform raise_exception('1d2h3m4s'::interval, null, null, null, null);
                 elsif i = 6 then
-                    perform raise_exception(now());
+                    perform raise_exception(now(), null, null, null, null, null);
                 elsif i = 7 then
-                    perform raise_exception(true);
+                    perform raise_exception(true, null, null, null, null, null, null);
                 elsif i = 8 then
-                    perform raise_exception(-123.456);
+                    perform raise_exception(-123.456, null, null, null, null, null, null, null);
+                elsif i = 9 then
+                    perform raise_exception(point(0, 0), null, null, null, null, null, null, null, null);
                 end if;
                 EXIT WHEN true;
             EXCEPTION WHEN others THEN
-                GET STACKED DIAGNOSTICS
+                GET STACKED DIAGNOSTICS --https://postgrespro.ru/docs/postgresql/14/plpgsql-control-structures#PLPGSQL-ERROR-TRAPPING
                     exception_sqlstate := RETURNED_SQLSTATE,
                     exception_message  := MESSAGE_TEXT,
                     exception_context  := PG_EXCEPTION_CONTEXT,
                     exception_detail   := PG_EXCEPTION_DETAIL,
-                    exception_hint     := PG_EXCEPTION_HINT;
+                    exception_hint     := PG_EXCEPTION_HINT,
+                    exception_datatype := PG_DATATYPE_NAME;
 
                 RAISE NOTICE '====== % ======', i;
                 RAISE NOTICE '* exception_sqlstate = %', exception_sqlstate;
@@ -74,6 +85,7 @@ do $$
                 RAISE NOTICE '* exception_context = %', exception_context;
                 RAISE NOTICE '* exception_detail = %', exception_detail;
                 RAISE NOTICE '* exception_hint = %', exception_hint;
+                RAISE NOTICE '* exception_datatype = %', exception_datatype;
             END;
         END LOOP;
     END;
