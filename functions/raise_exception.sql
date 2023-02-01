ef798f87d9a55f461c38734c203c10ba8d5b5e26
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-02-01 11:37:45 +0300

    USE EXAMPLES

--- functions/raise_exception.sql
+++ functions/raise_exception.sql
@@ -31,12 +31,12 @@ begin
         constraint  = coalesce("constraint", ''),
         table       = coalesce("table", ''),
         schema      = coalesce("schema", ''),
-        datatype    = pg_typeof(value);
+        datatype    = pg_typeof(value)::text;
     return null::bool;
 end;
 $$;
 
---TEST
+--TEST FUNCTION
 do $$
     DECLARE
         i int not null default 0;
@@ -92,15 +92,25 @@ do $$
     END;
 $$;
 
-/*
---explain
+------------------------------------------------------------------------------------------------------------------------
+--USE EXAMPLE 1
+select case finger
+            when 1 then 'one'
+            when 2 then 'two'
+            when 3 then 'three'
+            when 4 then 'four'
+            when 5 then 'five'
+            else raise_exception(finger)::text
+       end
+from unnest(array[1, 2, 3, 4, 5]::int[]) as hand(finger);
+
+--USE EXAMPLE 2
 select i
-from generate_series(1, 3000000) as x(i)
+from generate_series(1, 300) as x(i)
 where case when clock_timestamp() - statement_timestamp() < '1s'
            then true
            else raise_exception(i)
       end
 order by i;
-*/
 
---https://stackoverflow.com/questions/24882630/equivalent-or-alternative-method-for-raise-exception-statement-for-function-in-l
+--TODO https://stackoverflow.com/questions/24882630/equivalent-or-alternative-method-for-raise-exception-statement-for-function-in-l
