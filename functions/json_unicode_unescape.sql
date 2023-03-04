c6a5faf451c928bbd7234dcc1978cac50f8fb63a
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-03-05 02:21:46 +0300

    Create json_unicode_unescape.sql

---
+++ functions/json_unicode_unescape.sql
@@ -0,0 +1,24 @@
+create or replace function json_unicode_unescape(json)
+    returns json
+    immutable
+    returns null on null input
+    parallel safe -- Postgres 10 or later
+    language sql
+    set search_path = ''
+as
+$func$
+    select $1::jsonb::json;
+$func$;
+
+comment on function json_unicode_unescape(json) is $$
+    Evaluate escaped Unicode characters in the argument.
+    Unicode characters can be specified as \uXXXX (4 hexadecimal digits).
+    Hint: convert json column to jsonb.
+$$;
+
+
+do $$
+begin
+    assert json_unicode_unescape('"''\u017D\u010F\u00E1r, \\Нello\r\n\t \u270C, Привет! \ud83d\udc18\ud83d\ude03"'::json)::text = '"''Žďár, \\Нello\r\n\t ✌, Привет! 🐘😃"';
+end;
+$$;
