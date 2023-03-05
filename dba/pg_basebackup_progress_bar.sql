e0f8504d1fb332b2d4b449281af025e0881f93cb
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-03-06 01:03:18 +0300

    Create pg_basebackup_progress_bar.sql

---
+++ dba/pg_basebackup_progress_bar.sql
@@ -0,0 +1,22 @@
+--pg_basebackup progress bar, copy speed, estimated duration, estimated datetime finish
+
+select pg_size_pretty(b.backup_streamed) as pretty_backup_streamed,
+       pg_size_pretty(b.backup_total) as pretty_backup_total,
+       a.query_start,
+       e.duration,
+       round(e.progress_percent, 4) as progress_percent,
+       bytes_per_second,
+       (e2.estimated_duration || 'sec')::interval as estimated_duration,
+       a.query_start + (e2.estimated_duration || 'sec')::interval as estimated_query_end
+from pg_stat_progress_basebackup as b
+inner join pg_stat_activity as a on a.pid = b.pid
+cross join lateral (
+    select
+        NOW() - a.query_start as duration,
+        b.backup_streamed * 100.0 / b.backup_total as progress_percent
+) as e
+cross join lateral (
+    select
+        EXTRACT(epoch FROM e.duration) * 100 / e.progress_percent as estimated_duration,
+        round(b.backup_streamed / EXTRACT(epoch FROM e.duration)) as bytes_per_second
+) as e2;
