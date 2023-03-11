af9bb178aacef56a985a7e804faa794c76696509
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-03-11 21:16:46 +0300

    tables_diff.sql

---
+++ functions/tables_diff.sql
@@ -0,0 +1,24 @@
+/*
+ Take two table names (through the `regtable` type),
+ build a query comparing their contents, execute it, and
+ return a set of diff-like results with the rows that differ.
+*/
+CREATE FUNCTION diff_tables(t1 regclass, t2 regclass) 
+ returns TABLE("+/-" text, line text)
+AS $func$
+BEGIN
+  RETURN QUERY EXECUTE format($$
+   SELECT '+', d1.*::text FROM (
+    SELECT * FROM %s
+     EXCEPT
+    SELECT * FROM %s) AS d1
+   UNION ALL
+   SELECT '-', d2.*::text FROM (
+    SELECT * FROM %s
+     EXCEPT
+    SELECT * FROM %s) AS d2
+   $$, t2, t1, t1, t2);
+END
+$func$ language plpgsql;
+
+-- Source: https://github.com/dverite/postgresql-functions/blob/master/diff/diff-tables.sql
