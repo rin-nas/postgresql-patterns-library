38507107518731afe77d5dc8c88690e1e1b95cdf
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-04-10 21:12:58 +0300

    `public.` added

--- procedures/loop_execute/using_examples.sql
+++ procedures/loop_execute/using_examples.sql
@@ -1,7 +1,7 @@
 --деперсонализация (обезличивание) персональных данных (email)
 call loop_execute(
-    'person_email',
-    $$
+    'public.person_email', --table_name
+    $query$
         /*
         MATERIALIZED позволяет сначала вычислить все тяжёлые вычисления, а потом модифицировать строки.
         В этом случае длительность блокирования строк будет меньше, это видно в плане запроса.
@@ -9,7 +9,7 @@ call loop_execute(
         WITH s (id, value) AS MATERIALIZED (
             SELECT id,
                    hash_email_username(email, id)
-            FROM person_email
+            FROM public.person_email
             WHERE id > $1
               --AND use_parallel(id, 1, 1)
               AND email IS NOT NULL AND TRIM(email) != ''
@@ -18,7 +18,7 @@ call loop_execute(
             LIMIT $2 OFFSET $3
         ),
         m AS (
-            UPDATE person_email AS u
+            UPDATE public.person_email AS u
             SET email = s.value
             FROM s
             WHERE s.id = u.id
@@ -26,21 +26,21 @@ call loop_execute(
         SELECT MAX(id)  AS stop_id,
                COUNT(*) AS affected_rows
         FROM s;
-    $$
+    $query$
 );
 
 ------------------------------------------------------------------------------------------------------------------------
 -- удаление невалидных email
 call loop_execute(
-    'person_email',
-    $$
+    'public.person_email', --table_name
+    $query$
         /*
         MATERIALIZED позволяет сначала вычислить все тяжёлые вычисления, а потом модифицировать строки.
         В этом случае длительность блокирования строк будет меньше, это видно в плане запроса.
         */
         WITH s AS MATERIALIZED (
             SELECT id
-            FROM person_email
+            FROM public.person_email
             WHERE id > $1
               --AND use_parallel(id, 1, 1)
               AND email IS NOT NULL -- skip NULL
@@ -54,11 +54,11 @@ call loop_execute(
             LIMIT $2 OFFSET $3
         ),
         m AS (
-            DELETE FROM person_email AS d
+            DELETE FROM public.person_email AS d
             WHERE id IN (SELECT id FROM s)
         )
         SELECT MAX(id)  AS stop_id,
                COUNT(*) AS affected_rows
         FROM s;
-    $$
+    $query$
 );
