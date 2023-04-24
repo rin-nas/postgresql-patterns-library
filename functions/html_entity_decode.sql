d3390c6a22eeb5d1878fede6fe75a17505a315d4
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-04-24 16:43:02 +0300

    Update html_entity_decode.sql

--- functions/html_entity_decode.sql
+++ functions/html_entity_decode.sql
@@ -1,11 +1,16 @@
-create or replace function html_entity_decode(s text)
-returns text
-language plpgsql
+create or replace function html_entity_decode(str text)
+    returns text
+    immutable
+    returns null on null input
+    parallel safe -- postgres 10 or later
+    language plpgsql
+    set search_path = ''
 as $func$
     declare
+        -- Last Updated 24 April 2023
         -- https://html.spec.whatwg.org/multipage/named-characters.html
         -- https://html.spec.whatwg.org/entities.json
-        map constant jsonb not null default $${
+        entities constant jsonb not null default $${
   "&AElig": { "codepoints": [198], "characters": "\u00C6" },
   "&AElig;": { "codepoints": [198], "characters": "\u00C6" },
   "&AMP": { "codepoints": [38], "characters": "\u0026" },
@@ -2238,40 +2243,85 @@ as $func$
   "&zwj;": { "codepoints": [8205], "characters": "\u200D" },
   "&zwnj;": { "codepoints": [8204], "characters": "\u200C" }
 }$$;
-        r record;
-        esc constant char(1) not null default e'\u0001';
+        rec record;
+        protect_char constant char(1) not null default e'\u0001'; --protect incorrect chain process, like &amp;quot
+
+        /*
+        Protect chr() errors:
+            [54000] ERROR: null character not permitted
+            [54000] ERROR: requested character not valid for encoding: DDDDD
+        The maximum valid code point in Unicode is U+10FFFF
+        There are also a set of code points that are the surrogates for UTF-16. These are in the range U+D800 .. U+DFFF.
+        Details at https://stackoverflow.com/questions/27415935/does-unicode-have-a-defined-maximum-number-of-code-points
+        */
+        codepoints_ranges constant int4range[] not null default array[
+            int4range(1,            x'D800'::int,   '[)'),
+            int4range(x'DFFF'::int, x'10FFFF'::int, '(]')
+        ];
     begin
-        for r in
+        for rec in
             select distinct
-                m[1] as name
-            from regexp_matches(s, '&[a-zA-Z][a-zA-Z\d]+;?', 'g') as m
-            where map->m[1] is not null
+                r.m[1] as name
+            from regexp_matches(str, '&[a-zA-Z][a-zA-Z\d]+;?', 'g') as r(m)
+            where entities->r.m[1] is not null
         loop
-            s := replace(s, r.name, esc || (map->r.name->>'characters'));
+            str := replace(str, rec.name, concat(protect_char, (entities->rec.name->>'characters'), protect_char));
         end loop;
 
-        for r in
+        for rec in
             select distinct
-                m[1] as name,
-                m[2]::int as codepoint
-            from regexp_matches(s, '(&#([0-9]{1,10});)', 'g') as m
+                r.m[1] as name,
+                r.m[2]::int as codepoint
+            from regexp_matches(str, '(&#(\d{1,7});)', 'g') as r(m) -- maximum valid code point in Unicode is 1114111 (U+10FFFF)
+            where r.m[2]::int <@ any (codepoints_ranges) -- https://postgrespro.ru/docs/postgresql/12/functions-range
         loop
-            s := replace(s, r.name, esc || chr(r.codepoint));
+            str := replace(str, rec.name, concat(protect_char, chr(rec.codepoint), protect_char));
         end loop;
 
-        for r in
+        for rec in
             select distinct
-                m[1] as name,
-                ('x' || repeat('0', 8 - octet_length(m[2])) || m[2])::bit(32)::int as codepoint
-            from regexp_matches(s, '(&#x([\da-fA-F]{1,8});)', 'g') as m
+                r.m[1] as name,
+                x.codepoint
+            from regexp_matches(str, '(&#x([\da-fA-F]{1,6});)', 'g') as r(m) -- maximum valid code point in Unicode is U+10FFFF
+            -- https://stackoverflow.com/questions/8316164/convert-hex-in-text-representation-to-decimal-number
+            cross join lateral (select ('x' || lpad(r.m[2], 8, '0'))::bit(32)::int) as x(codepoint)
+            where codepoint <@ any (codepoints_ranges) -- https://postgrespro.ru/docs/postgresql/12/functions-range
         loop
-            s := replace(s, r.name, esc || chr(r.codepoint));
+            str := replace(str, rec.name, concat(protect_char, chr(rec.codepoint), protect_char));
         end loop;
 
-        return replace(s, esc, '');
+        return replace(str, protect_char, '');
     end;
 $func$;
 
--- select html_entity_decode('&#9632; 1 &Afr; 2 &acE; 3 &#x25a0; &frac12;');
+comment on function html_entity_decode(str text) is 'Convert HTML entities to their corresponding characters';
 
--- https://stackoverflow.com/questions/14961992/postgresql-replace-html-entities-function
+-- TEST
+
+DO $$
+DECLARE
+    rec record;
+    str_out_returned text;
+BEGIN
+    for rec in select * from (values
+        (null, null),
+
+        -- Positive
+        ('&Afr; &acE; &frac12; &quot &amp, &amp;quot, &gt; &Ouml; &Bopf;', '𝔄 ∾̳ ½ " &, &quot, > Ö 𝔹'), --Named entities
+        ('&#34; &#055; &#120171; &#1114111;', '" 7 𝕫 􏿿'), --Dec code entities
+        ('&#x25a0; &#x02DC; &#x1D539; &#x10FFFF;', '■ ˜ 𝔹 􏿿'), --Hex code entities
+
+        -- Negative
+        ('&unknown; &unk', '&unknown; &unk'), --Named entities
+        ('&#0; &#55296; &#57343; &#1234567; &#9999999; &#05_5', '&#0; &#55296; &#57343; &#1234567; &#9999999; &#05_5'), --Dec code entities
+        ('&#x0; &#xD800; &#xDFFF; &#x12d687; &#xffffff; &#x25g0;', '&#x0; &#xD800; &#xDFFF; &#x12d687; &#xffffff; &#x25g0;') --Hex code entities
+    ) as t(str_in, str_out_expected)
+    loop
+        str_out_returned = html_entity_decode(rec.str_in);
+        ASSERT
+            -- результат сравнения должен вернуть boolean
+            str_out_returned is not distinct from rec.str_out_expected,
+            -- если результат сравнения не true, то вернётся сообщение с ошибкой
+            format('in %L, out expected %L, out returned %L', rec.str_in, rec.str_out_expected, str_out_returned);
+    end loop;
+END $$;
