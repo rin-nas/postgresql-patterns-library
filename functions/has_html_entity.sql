796ac7259050065dbdd0244d662bbf231f0a4ed2
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-04-24 12:33:39 +0300

    Update has_html_entity.sql

--- functions/has_html_entity.sql
+++ functions/has_html_entity.sql
@@ -11,8 +11,8 @@ $$
 select position('&' in str) > 0 --speed improves
        and regexp_match(
             str,
-            --https://stackoverflow.com/questions/15532252/why-is-reg-being-rendered-as-without-the-bounding-semicolon
-            --https://html.spec.whatwg.org/multipage/named-characters.html#named-character-references
+            -- https://stackoverflow.com/questions/15532252/why-is-reg-being-rendered-as-without-the-bounding-semicolon
+            -- https://html.spec.whatwg.org/multipage/named-characters.html TODO update regexp entities list from last version
             $regexp$
             (&
             (?:  (?:AElig|AMP|Aacute|Acirc|Agrave|Aring|Atilde|Auml|COPY|Ccedil|ETH|Eacute|Ecirc|Egrave|Euml|GT|Iacute|Icirc|Igrave|Iuml|LT|Ntilde|Oacute|Ocirc|Ograve|Oslash|Otilde|Ouml|QUOT|REG|THORN|Uacute|Ucirc|Ugrave|Uuml|Yacute|aacute|acirc|acute|aelig|agrave|amp|aring|atilde|auml|brvbar|ccedil|cedil|cent|copy|curren|deg|divide|eacute|ecirc|egrave|eth|euml|frac12|frac14|frac34|gt|iacute|icirc|iexcl|igrave|iquest|iuml|laquo|lt|macr|micro|middot|nbsp|not|ntilde|oacute|ocirc|ograve|ordf|ordm|oslash|otilde|ouml|para|plusmn|pound|quot|raquo|reg|sect|shy|sup1|sup2|sup3|szlig|thorn|times|uacute|ucirc|ugrave|uml|uuml|yacute|yen|yuml) #name
