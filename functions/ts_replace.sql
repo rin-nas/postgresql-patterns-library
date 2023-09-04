54c80516762174dd6962f9a9de98c8c18deb2f71
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-09-04 19:29:04 +0300

    Create ts_replace.sql

---
+++ functions/ts_replace.sql
@@ -0,0 +1,51 @@
+create or replace function public.ts_replace(
+    vector tsvector,
+    str_from text,
+    str_to text
+)
+    returns tsvector
+    stable
+    returns null on null input
+    parallel safe
+    language sql
+    set search_path = ''
+    cost 1
+as
+$function$
+
+with s as (
+    select case when lexeme = str_from then str_to else lexeme end as new_lexeme,
+           unnest(positions) as position,
+           unnest(weights) as weight
+    from unnest(vector) as t(lexeme, positions, weights)
+)
+--https://www.postgrespro.ru/docs/postgresql/12/datatype-textsearch#DATATYPE-TSVECTOR
+select array_to_string(array(
+   select concat(
+                 concat($$'$$, replace(replace(new_lexeme, $$'$$, $$''$$), $$\$$, $$\\$$), $$'$$),
+                 ':',
+                 string_agg(concat(position, weight), ',')
+               )
+   from s
+   group by new_lexeme
+), ' ')::tsvector;
+
+$function$;
+
+comment on function public.ts_replace(vector tsvector, str_from text, str_to text) is 'Заменяет заданную лексему в векторе';
+
+--TEST
+DO $do$
+    BEGIN
+        --positive
+        assert public.ts_replace($$'test':9,2$$::tsvector, 'test', 'abcd') = $$'abcd':2,9$$::tsvector;
+        assert public.ts_replace($$'\\te''st':9,2$$::tsvector, $$\te'st$$, $$\AB'CD$$) = $$'\\AB''CD':2,9$$::tsvector;
+        assert public.ts_replace($$'test':9A,2B,13 'база':7D$$::tsvector, 'test', 'база') = $$'база':2B,7,9A,13$$::tsvector;
+
+        --negative
+        assert public.ts_replace($$'testing':9,2$$::tsvector, 'test', 'abcd') = $$'testing':2,9$$::tsvector;
+        assert public.ts_replace($$'protest':9,2$$::tsvector, 'test', 'abcd') = $$'protest':2,9$$::tsvector;
+        assert public.ts_replace($$'test''ing':9A,2B,13 'база':7$$::tsvector, 'test', 'abcd') = $$'test''ing':9A,2B,13 'база':7$$::tsvector;
+        assert public.ts_replace($$'protest':9,2$$::tsvector, 'test', null) is null;
+    END;
+$do$;
