5020dbd9bb8600e071ec40240f40c680214eae28
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-09-26 23:01:39 +0300

    public. added

--- domains/css_color.sql
+++ domains/css_color.sql
@@ -1,13 +1,13 @@
 -- https://developer.mozilla.org/en-US/docs/Web/CSS/color_value
 -- https://regex101.com/r/CMQKwv/3/
 
-CREATE DOMAIN css_color AS text CHECK(
+CREATE DOMAIN public.css_color AS text CHECK(
     octet_length(VALUE) BETWEEN 4 AND 9 
     AND VALUE ~ '^#[a-fA-F\d]{3}(?:[a-fA-F\d]{3})?$|^#[a-fA-F\d]{4}(?:[a-fA-F\d]{4})?$'
 );
 
-COMMENT ON DOMAIN css_color IS 'CSS color';
+COMMENT ON DOMAIN public.css_color IS 'CSS color';
 
 --TEST
-select '#777'::css_color; --ok
-select '$777'::css_color; --error
+select '#777'::public.css_color; --ok
+select '$777'::public.css_color; --error
