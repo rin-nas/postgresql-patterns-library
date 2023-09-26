e9f44012c5170f9c675f54c77fff4ac048ef4186
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-09-26 23:08:16 +0300

    public. added

--- domains/inn10.sql
+++ domains/inn10.sql
@@ -1,2 +1,2 @@
-CREATE DOMAIN inn10 AS text CHECK(is_inn10(VALUE));
-COMMENT ON DOMAIN inn10 IS 'ИНН юридического лица';
+CREATE DOMAIN public.inn10 AS text CHECK(public.is_inn10(VALUE));
+COMMENT ON DOMAIN public.inn10 IS 'ИНН юридического лица';
