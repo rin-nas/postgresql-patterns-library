28f91dd0e5605c986e5d771c18fcb05883098f30
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-09-26 23:07:54 +0300

    public. added

--- domains/inn.sql
+++ domains/inn.sql
@@ -1,18 +1,18 @@
-CREATE DOMAIN inn AS text CHECK(is_inn10(VALUE) OR is_inn12(VALUE));
-COMMENT ON DOMAIN inn IS 'ИНН юридического или физического лица или ИП';
+CREATE DOMAIN public.inn AS text CHECK(public.is_inn10(VALUE) OR public.is_inn12(VALUE));
+COMMENT ON DOMAIN public.inn IS 'ИНН юридического или физического лица или ИП';
 
 --TESTS
-select '7725088527'::inn; --ok
-select '7715034360'::inn10; --ok
-select '773370857141'::inn; --ok
-select '344809916052'::inn12; --ok
+select '7725088527'::public.inn; --ok
+select '7715034360'::public.inn10; --ok
+select '773370857141'::public.inn; --ok
+select '344809916052'::public.inn12; --ok
 
-select '1234567890'::inn; --error
-select '123456789012'::inn; --error
-select '1234567890'::inn10; --error
-select '123456789012'::inn12; --error
+select '1234567890'::public.inn; --error
+select '123456789012'::public.inn; --error
+select '1234567890'::public.inn10; --error
+select '123456789012'::public.inn12; --error
 
-select '12345678901'::inn; --error
-select '1234567890123'::inn; --error
-select '12345678901'::inn10; --error
-select '1234567890123'::inn12; --error
+select '12345678901'::public.inn; --error
+select '1234567890123'::public.inn; --error
+select '12345678901'::public.inn10; --error
+select '1234567890123'::public.inn12; --error
