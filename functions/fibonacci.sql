4470ab23349f466e45fce6a65d106fa89e560c35
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-10-05 14:10:40 +0300

    Update fibonacci.sql

--- functions/fibonacci.sql
+++ functions/fibonacci.sql
@@ -39,4 +39,8 @@ do $$
     end;
 $$;
 
---see also https://wiki.postgresql.org/wiki/Fibonacci_Numbers
+/*
+See also:
+https://wiki.postgresql.org/wiki/Fibonacci_Numbers
+https://stackoverflow.com/questions/37479718/how-to-turn-integers-into-fibonacci-coding-efficiently
+*/
