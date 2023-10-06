fed470b70a7e8cecd6ec1e43692eb524c434634a
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2023-10-06 11:47:21 +0300

    Update do_3.table.sql

--- functions/db_validation/do_3.table.sql
+++ functions/db_validation/do_3.table.sql
@@ -8,9 +8,10 @@ comment on type db_validation.schema_validate_checks is $$
     has_index_for_fk        - наличие индексов для ограничений внешних ключей в таблице
     has_table_comment       - наличие описания для таблицы
     has_column_comment      - наличие описания для колонки
-    is_table_column_comment_unique - описания колонок в таблице должны быть уникальными
-    check_table_name
-    check_column_name
+    TODO:
+        is_table_column_comment_unique - в таблице описания колонок должны быть уникальными
+        check_table_name    - проверять названия таблиц на соответствие регулярному выражению
+        check_column_name   - проверять названия колонок на соответствие регулярному выражению
 $$;
 
 -- alter type db_validation.schema_validate_checks owner to alexan;
