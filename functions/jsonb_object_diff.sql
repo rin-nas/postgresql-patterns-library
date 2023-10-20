4aadb49597fc3d231b4ba475e6d283290162edb7
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-10-20 18:29:07 +0300

    jsonb_diff() => jsonb_object_diff()

--- functions/jsonb_object_diff.sql
+++ functions/jsonb_object_diff.sql
@@ -1,13 +1,13 @@
-CREATE OR REPLACE FUNCTION jsonb_diff(l JSONB, r JSONB) RETURNS JSONB
+CREATE OR REPLACE FUNCTION public.jsonb_object_diff(l JSONB, r JSONB) RETURNS JSONB
     LANGUAGE sql
     set search_path = ''
-AS $json_diff$
+AS $func$
     SELECT jsonb_object_agg(a.key, a.value)
     FROM (SELECT key, value FROM jsonb_each(l)) AS a(key,value)
     LEFT OUTER JOIN (SELECT key, value FROM jsonb_each(r)) b(key,value) ON a.key = b.key
     WHERE a.value != b.value OR b.key IS NULL;
-$json_diff$;
+$func$;
 
-SELECT jsonb_diff('{"a":1,"b":2}'::JSONB, '{"a":1,"b":null}'::JSONB);
+SELECT public.jsonb_object_diff('{"a":1,"b":2}'::JSONB, '{"a":1,"b":null}'::JSONB);
 
 --Hstore style delete "-" operator for jsonb: https://github.com/glynastill/pg_jsonb_delete_op
