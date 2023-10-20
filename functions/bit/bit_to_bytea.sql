046507684fc9e1937b83de10b30ea2684f698667
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-10-20 23:27:31 +0300

    bit_to_bytea(bits bit varying) added

---
+++ functions/bit/bit_to_bytea.sql
@@ -0,0 +1,25 @@
+create or replace function public.bit_to_bytea(bits bit varying)
+    returns bytea
+    immutable
+    strict -- returns null if any parameter is null
+    parallel safe -- Postgres 10 or later
+    security invoker
+    language sql
+    set search_path = ''
+AS $func$
+    select public.bytea_agg(
+                decode(lpad(to_hex(substring(bits8 from i * 8 + 1 for 8)::int), 2, '0'), 'hex')
+           )
+    from octet_length(bits) as len
+    cross join generate_series(0, len - 1) as i
+    cross join public.bit_rpad(bits, len * 8, B'0') as bits8;
+$func$;
+
+comment on function public.bit_to_bytea(bits bit varying) is 'Converts bit varying to bytea type';
+
+--TEST
+do $$
+    begin
+        assert public.bit_to_bytea(B'1101100111011000111001101011000011100011010011') = '\xD9D8E6B0E34C';
+    end;
+$$;
