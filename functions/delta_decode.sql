6ed11e7826d492b52a0ec9d837654fc940ebecc3
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-10-22 20:06:40 +0300

    renames `array_delta_*()` to `delta_*()`

--- functions/delta_decode.sql
+++ functions/delta_decode.sql
@@ -1,4 +1,4 @@
-create or replace function array_delta_decode(a int[])
+create or replace function public.delta_decode(a int[])
     returns int[]
     immutable
     returns null on null input
@@ -9,12 +9,12 @@ as
 $func$
     select array(
         select sum(a.v) over (order by a.o rows between unbounded preceding and current row)
-        from unnest(array_delta_decode.a) with ordinality as a(v, o)
+        from unnest(delta_decode.a) with ordinality as a(v, o)
         order by a.o
     )
 $func$;
 
-comment on function array_delta_decode(a int[]) is 'https://en.wikipedia.org/wiki/Delta_encoding';
+comment on function public.delta_decode(a int[]) is 'https://en.wikipedia.org/wiki/Delta_encoding';
 
 -- TEST
-select array_delta_decode(array[2, 2, 2, 3, -2]) = array[2, 4, 6, 9, 7];
+select public.delta_decode(array[2, 2, 2, 3, -2]) = array[2, 4, 6, 9, 7];
---
+++ functions/delta_encode.sql
@@ -0,0 +1,20 @@
+create or replace function public.delta_encode(a int[])
+    returns int[]
+    immutable
+    returns null on null input
+    parallel safe -- Postgres 10+
+    language sql
+    set search_path = ''
+as
+$func$
+    select array(
+        select coalesce(a.v - lag(a.v) over (order by a.o), a.v)
+        from unnest(delta_encode.a) with ordinality as a(v,o)
+        order by a.o
+    )
+$func$;
+
+comment on function public.delta_encode(a int[]) is 'https://en.wikipedia.org/wiki/Delta_encoding';
+
+-- TEST
+select public.delta_encode(array[2, 4, 6, 9, 7]) = array[2, 2, 2, 3, -2];
