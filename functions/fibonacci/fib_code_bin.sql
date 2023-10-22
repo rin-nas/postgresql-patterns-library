b823af08e5a95152c64bc66279760b764fadc605
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-10-22 19:40:45 +0300

    fib_code_bin() test added, comments improved

--- functions/fibonacci/fib_code_bin.sql
+++ functions/fibonacci/fib_code_bin.sql
@@ -8,11 +8,30 @@ create or replace function public.fib_code_bin(n int)
     set search_path = ''
 as $func$
     select public.bin(
-               public.bit_reverse(n) << 1 | 1,
+               public.bit_reverse(n) << 1 | 1, --add bit 1 to end
                bit_length(public.bin(n)) + 1
            )
 $func$;
 
 comment on function public.fib_code_bin(n int) is $$
     Converts Fibonacci integer type to universal code as bit type.
+    https://en.wikipedia.org/wiki/Fibonacci_coding
+    Example:
+    dec => fib_code_bin
+    1 => 11
+    2 => 011
+    3 => 0011
+    4 => 1011
+    5 => 00011
+    6 => 10011
+    7 => 01011
+    8 => 000011
+    9 => 100011
+    ...
 $$;
+
+--TEST
+select dec,
+       fib_code_bin(fib)
+from generate_series(1, 500) as dec
+cross join fib_encode(dec) as fib;
