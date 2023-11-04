5e011d0f3d65931e7a1955963390aba1bf3fc534
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-11-04 17:48:59 +0300

    fib_pack() tests improved

--- functions/fibonacci/fib_pack.sql
+++ functions/fibonacci/fib_pack.sql
@@ -18,11 +18,12 @@ as $func$
          coalesce(array(select t.n from public.fib_seq(47) as t(n) offset 2)) as f(seq); --1 2 3 5 8 13 21...
 $func$;
 
-comment on function public.fib_pack(a int[]) is 'Packs integers with values > 0 by Fibonacci encoding algorithm';
+comment on function public.fib_pack(a int[]) is 'Packs integers (with values > 0) by Fibonacci encoding algorithm to bytea';
 
 --TEST
 do $$
     begin
-        assert public.fib_pack('{1,2,3,4,5,6}'::int[]) = '\xd9d8e6';
+        assert public.fib_pack('{1,2,3,4,5,6,7,8,9,10,11,12,13}') = '\xD9D8E6B0E34CBAC180';
+        assert public.fib_pack('{13,12,11,10,9,8,7,6,5,4,3,2,1}') = '\x075969C61AE63B3780';
     end;
 $$;
