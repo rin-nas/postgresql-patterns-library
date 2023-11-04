adac6139307f9f610281475914a94fcef60dfe30
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-11-04 18:07:56 +0300

    fib_pack() improved for empty

--- functions/fibonacci/fib_pack.sql
+++ functions/fibonacci/fib_pack.sql
@@ -7,12 +7,15 @@ create or replace function public.fib_pack(a int[])
     language sql
     set search_path = ''
 as $func$
-    select public.bit_to_bytea(
-                public.bit_agg(
-                    public.fib_code_bin(
-                        public.fib_encode(u.dec, f.seq)
-                    )
-                )
+    select coalesce(
+               public.bit_to_bytea(
+                   public.bit_agg(
+                       public.fib_code_bin(
+                           public.fib_encode(u.dec, f.seq)
+                       )
+                   )
+               ),
+               ''
            )
     from unnest(a) as u(dec),
          coalesce(array(select t.n from public.fib_seq(47) as t(n) offset 2)) as f(seq); --1 2 3 5 8 13 21...
@@ -23,6 +26,9 @@ comment on function public.fib_pack(a int[]) is 'Packs integers (with values > 0
 --TEST
 do $$
     begin
+        assert public.fib_pack(null) is null;
+        assert public.fib_pack('{}') = '\x';
+
         assert public.fib_pack('{1,2,3,4,5,6,7,8,9,10,11,12,13}') = '\xD9D8E6B0E34CBAC180';
         assert public.fib_pack('{13,12,11,10,9,8,7,6,5,4,3,2,1}') = '\x075969C61AE63B3780';
     end;
