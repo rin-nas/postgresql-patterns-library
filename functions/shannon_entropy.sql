e8c969fc5a5ac40602c3b2db7ef74414ec53ea51
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-11-08 19:55:30 +0300

    shannon_entropy() improved

--- functions/shannon_entropy.sql
+++ functions/shannon_entropy.sql
@@ -7,18 +7,25 @@ create or replace function public.shannon_entropy(s text)
     language sql
     set search_path = ''
 as $func$
-    with f(f) as (
+    with t(freq) as (
         select count(*) * 1.0 / l.l
-        from regexp_split_to_table(shannon_entropy.s, '') as c(c)
-           , length(shannon_entropy.s) as l(l)
-        group by c.c, l.l
+        --from regexp_split_to_table(shannon_entropy.s, '') as s(char)
+        from unnest(string_to_array(shannon_entropy.s, null)) as s(char)
+           , char_length(shannon_entropy.s) as l(l)
+        group by s.char, l.l
     )
-    select -1 * sum(f.f * log(2, f.f))
-    from f
+    select -1 * sum(t.freq * log(2, t.freq))
+    from t
 $func$;
 
 comment on function public.shannon_entropy(s text) is $$
 Calculates Shannon entropy.
+Возвращает число >= 0.
+
+Энтропия - это среднее количество собственной информации в данных.
+Энтропия рассматривается как мера беспорядка в данных.
+За единицу количества информации принимается 1 бит (да/нет).
+
 Энтропия ограничивает максимально возможное сжатие информации без потерь.
 Согласно теореме Шеннона, существует предел сжатия без потерь, зависящий от энтропии источника.
 Чем более предсказуемы получаемые данные, тем лучше их можно сжать.
@@ -29,10 +36,16 @@ $$;
 -- TEST
 do $$
     begin
+        --отсортировано по возрастанию энтропии
+        assert public.shannon_entropy('z') = 0;
         assert public.shannon_entropy('zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz') = 0;
+        assert public.shannon_entropy('AB') = 1;
+        assert public.shannon_entropy('AABB') = 1;
         assert public.shannon_entropy('AABC') = 1.5;
-        assert round(public.shannon_entropy('0123456789'), 2) = 3.32;
         assert round(public.shannon_entropy('abracadabra'), 2) = 2.04;
+        assert round(public.shannon_entropy('абракадабра'), 2) = 2.04;
+        assert round(public.shannon_entropy('0123456789'), 2) = 3.32;
+        assert round(public.shannon_entropy('9876543210'), 2) = 3.32;
         assert round(public.shannon_entropy('Ехал Грека через реку. Видит Грека в реке рак. Сунул Грека руку в реку. Рак за руку Греку цап!'), 2) = 3.81;
     end;
 $$
