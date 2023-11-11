833e42cec61bfede469f0a014f2b40f9ca75dd22
Rinat Mukhtarov <r.mukhtarov@rabota.ru>
2023-11-11 17:46:27 +0300

    is_uuid(text) speed improved

--- functions/is/is_uuid.sql
+++ functions/is/is_uuid.sql
@@ -3,26 +3,18 @@ create or replace function public.is_uuid(str text)
     immutable
     returns null on null input
     parallel safe -- Postgres 10 or later
-    language plpgsql
+    language sql
     set search_path = ''
-    cost 5
 as
 $$
-begin
     --https://postgrespro.ru/docs/postgresql/12/datatype-uuid
-    if octet_length(str) not between 32 --a0eebc999c0b4ef8bb6d6bb9bd380a11
-                                 and 36 --a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11
-    then
-        return false;
-    end if;
-
-    return (
-        select s ~ '^[\da-fA-F]{32}$'
-               and s ~ '\d'
-               and s ~ '[a-fA-F]'
-        from replace(str, '-', '') as t(s)
-    );
-end
+    select case when octet_length(str) between 32 --a0eebc999c0b4ef8bb6d6bb9bd380a11
+                                           and 36 --a0eebc99-9c0b-4ef8-bb6d-6bb9bd380a11
+                then     str ~ '\d'
+                     and str ~ '[a-fA-F]'
+                     and replace(str, '-', '') ~ '^[\da-fA-F]{32}$'
+                else false
+           end;
 $$;
 
 comment on function public.is_uuid(text) is 'Проверяет, что переданная строка является UUID';
@@ -41,4 +33,3 @@ BEGIN
     assert not public.is_uuid('A0EEBC99-9C0B-4EF8-BB6D-6BB9BD380A-11');
 
 END $$;
-
