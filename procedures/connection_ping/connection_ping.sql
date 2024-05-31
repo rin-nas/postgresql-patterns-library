e821702b79e819c2abc9bba4b7af8e3e9c1870c4
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2024-05-31 13:16:25 +0300

    app_name added

--- procedures/connection_ping/connection_ping.sql
+++ procedures/connection_ping/connection_ping.sql
@@ -7,9 +7,10 @@ AS
 $$
 BEGIN
   FOR i IN 1..max_attempts LOOP
-    RAISE NOTICE 'ping %, client %:%, server (%) %:%',
+    RAISE NOTICE 'ping %, app_name %, client %:%, server (%) %:%',
       -- clock_timestamp()::timestamptz(3),
       i,
+	  current_setting('application_name'),
       inet_client_addr(), inet_client_port(),
       case when pg_is_in_recovery() then 'standby' else 'primary' end,
       inet_server_addr(), inet_server_port();
