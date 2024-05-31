ad530c8ac88123fa3e118a39a71d62e227273ebc
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2024-05-31 16:41:19 +0300

    Update psql_connection_lost_duration_v2.sh

--- procedures/connection_ping/psql_connection_lost_duration_v2.sh
+++ procedures/connection_ping/psql_connection_lost_duration_v2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #https://habr.com/ru/company/ruvds/blog/325522/ - Bash documentation
- 
+
 #https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 #set -euo pipefail #commented!
- 
+
 SCRIPT_FILE=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
- 
+
 # Check syntax this file
 bash -n "${SCRIPT_FILE}" || exit
- 
+
 # Colors
 Red='\e[1;31m'
 Green='\e[0;32m'
@@ -21,49 +21,50 @@ Cyan='\e[0;36m'
 Gray='\e[0;37m'
 White='\e[1;37m'
 Reset='\e[0m'
- 
+
 # Colored messages
 echoerr()  { echo -e "${Red}$@${Reset}"    1>&2; }
 echowarn() { echo -e "${Yellow}$@${Reset}" 1>&2; }
 echoinfo() { echo -e "${White}$@${Reset}" ; }
 echosucc() { echo -e "${Green}$@${Reset}" ; }
- 
+
 echoinfo "PostgreSQL connection lost duration test"
- 
+
 if [ ! -n "${1-}" ]
 then
     echoerr "Host is not defined as first parameter"
     echo "Usage: $SCRIPT_FILE <host> <port>"
     exit 2
 fi
- 
+
 if [ ! -n "${2-}" ]
 then
     echoerr "Port is not defined as second parameter"
     echo "Usage: $SCRIPT_FILE <host> <port>"
     exit 2
 fi
- 
+
 host=$1
 port=$2
- 
+
 cd $SCRIPT_DIR;
 
 # передаём в application_name основной IP текущего сервера, т.к. запрос может проходить через прокси
 # https://elephas.io/how-to-set-application_name-in-psql-command-line-utility/
 application_name="$(basename "$SCRIPT_FILE") $(hostname -I | cut -f1 -d' ')"
 export PGAPPNAME="$application_name"
- 
+
 # смотрим на подключение, пока не произойдёт разрыв
 psql -U postgres -q -X -c "\echo 'Press CTRL+C to stop'" -c "\conninfo" -f connection_ping.sql -c "call connection_ping(1000, 0.5)" -h $host -p $port
 status=$?
- 
+
 if test $status != 2; then
     echoerr "Error occured"
     exit $status
 fi
- 
+
 echowarn "Сonnection lost"
 echo "Press CTRL+C to stop"
 time_start=$(date +%s.%3N)
- 
+
+# произошёл разрыв, пытаемся подключиться
