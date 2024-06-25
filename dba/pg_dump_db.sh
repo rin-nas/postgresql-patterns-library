ef8aef0615493a0608c88ce164c1848d9b4a7ef2
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2024-06-25 15:59:02 +0300

    Create pg_dump_db.sh

---
+++ dba/pg_dump_db.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# https://habr.com/ru/company/ruvds/blog/325522/ - Bash documentation
+  
+# https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
+set -euo pipefail
+  
+SCRIPT_FILE=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_FILE")
+  
+# Check syntax this file
+bash -n "${SCRIPT_FILE}" || exit
+ 
+ 
+read -s -r -p "Enter password for postgres: " PGPASSWORD
+echo
+export PGPASSWORD
+ 
+ARCHIVE_DIR=/mnt/backup_db/tmp
+ 
+for dbname in my_db1 my_db2
+do
+    time ( \
+        pg_dump --verbose --username=postgres --host=localhost --dbname=${dbname} \
+            | zstd --verbose --adapt -f -q -o ${ARCHIVE_DIR}/${dbname}.sql.zst \
+    )
+done
+ 
+# zstd -l ${ARCHIVE_DIR}/*.sql.zst
