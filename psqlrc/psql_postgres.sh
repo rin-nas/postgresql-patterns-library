1a66e43b626804f5c5def3a81922e34ef9e5cda1
Rinat Mukhtarov <rin-nas@users.noreply.github.com>
2024-09-18 19:00:57 +0300

    Update psql_postgres.sh

--- psqlrc/psql_postgres.sh
+++ psqlrc/psql_postgres.sh
@@ -1,2 +1,2 @@
-# как войти без пароля (если в pg_hba.conf прописан метод peer) и подтянуть .psqlrc из своей домашней директории?
+# как войти под пользователем postgres без пароля (если в pg_hba.conf прописан метод peer) и подтянуть .psqlrc из своей домашней директории?
 cp -f ${HOME}/.psqlrc /tmp/.psqlrc && sudo su - postgres -c "export PSQLRC=/tmp/.psqlrc && psql -q"
