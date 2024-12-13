93b2491c6a97ed92ec31f86987a83f8fab825d1d
Rinat <rin-nas@users.noreply.github.com>
2024-12-13 11:18:58 +0300

    Update sql_pretty.sql

--- functions/sql_pretty.sql
+++ functions/sql_pretty.sql
@@ -1,7 +1,7 @@
 -- TODO
 select regexp_replace(
-    'select * from a inner join b where a.f = b.f order by a.id limit 10', 
-    '\s+(FROM|CROSS|LEFT|RIGHT|INNER|OUTER|JOIN|WHERE|HAVING|ON|GROUP BY|ORDER BY|LIMIT|SELECT|UNION|INTERSECT|EXCEPT)(\s)',
-    E'\n\\1\\2',
-    'gi'
-);
+  'select * from a inner join b where a.f = b.f order by a.id limit 10', 
+  '\s*\m(FROM|CROSS|LEFT|RIGHT|INNER|OUTER|JOIN|WHERE|HAVING|ON|GROUP BY|ORDER BY|LIMIT|SELECT|UNION|INTERSECT|EXCEPT|CASE|WHEN|ELSE|END)\M',
+  E'\n\\1',
+  'gi'
+) as query_pretty
