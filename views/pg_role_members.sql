d2b544344d2e507ceee47d146e3fd8aac2698f9e
Rinat <rin-nas@users.noreply.github.com>
2025-02-03 15:25:36 +0300

    Update pg_role_members.sql

--- views/pg_role_members.sql
+++ views/pg_role_members.sql
@@ -1,21 +1,23 @@
--- Source: https://www.cybertec-postgresql.com/en/postgresql-get-member-roles-and-permissions/
+-- Based on source: https://www.cybertec-postgresql.com/en/postgresql-get-member-roles-and-permissions/
 
 CREATE OR REPLACE VIEW pg_role_members AS
 WITH RECURSIVE x AS
 (
     SELECT member::regrole,
          roleid::regrole AS role,
-         member::regrole || ' -> ' || roleid::regrole AS path
+         member::regrole || ' -> ' || roleid::regrole AS path,
+         1 as depth
     FROM pg_auth_members AS m
     UNION ALL
     SELECT x.member::regrole,
          m.roleid::regrole,
-         x.path || ' -> ' || m.roleid::regrole
+         x.path || ' -> ' || m.roleid::regrole,
+         x.depth + 1
     FROM pg_auth_members AS m
     JOIN x ON m.member = x.role
 )
-SELECT member, role, path
+SELECT *
 FROM x
-ORDER BY member::text, role::text;
+ORDER BY member::text, depth, role::text;
   
 COMMENT ON VIEW pg_role_members IS 'Role membership tree';
