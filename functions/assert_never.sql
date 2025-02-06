c5c834a9b006a21e05a8cbcd2f89d2f04f463301
Rinat <rin-nas@users.noreply.github.com>
2025-02-06 19:32:03 +0300

    Update assert_never.sql

--- functions/assert_never.sql
+++ functions/assert_never.sql
@@ -1,6 +1,8 @@
 -- https://hakibenita.com/future-proof-sql#assert-never-in-sql
 -- https://docs.python.org/3.11/library/typing.html#typing.assert_never
 
+-- DEPRECATED, use raise_exception() instead
+
 CREATE OR REPLACE FUNCTION assert_never(v anyelement)
 RETURNS anyelement
 LANGUAGE plpgsql AS
