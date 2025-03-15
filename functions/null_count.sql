e65b5298037535af1867f200e8d96ec35ca6681d
Rinat Mukhtarov <rin-nas@ya.ru>
2025-03-15 13:38:27 +0300

    null_count()

---
+++ functions/null_count.sql
@@ -0,0 +1,21 @@
+create or replace function public.null_count(variadic anyarray)
+    returns integer
+    immutable
+    strict -- returns null if any parameter is null
+    parallel safe -- Postgres 10 or later
+    security invoker
+    language sql
+    set search_path = ''
+
+AS $function$
+    SELECT sum(CASE WHEN v IS NULL THEN 1 ELSE 0 END)::int FROM unnest($1) g(v);
+$function$
+
+/*
+--TEST
+CREATE TABLE xxx(
+  a int,
+  b int,
+  c int,
+  CHECK (null_count(a,b,c) <= 1));
+*/
