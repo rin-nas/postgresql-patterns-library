12f15f9b273d6ae779213e449714bce60d6de0ec
Rinat Mukhtarov <rin-nas@ya.ru>
2025-03-15 15:20:00 +0300

    indent

--- functions/encode/url_decode.sql
+++ functions/encode/url_decode.sql
@@ -8,17 +8,17 @@ CREATE OR REPLACE FUNCTION public.url_decode(input text)
     set search_path = ''
 AS $$
 DECLARE
- bin bytea = '';
- byte text;
+    bin bytea = '';
+    byte text;
 BEGIN
- FOR byte IN (select (regexp_matches(input, '(%..|.)', 'g'))[1]) LOOP
-   IF length(byte) = 3 THEN
-     bin = bin || decode(substring(byte, 2, 2), 'hex');
-   ELSE
-     bin = bin || byte::bytea;
-   END IF;
- END LOOP;
- RETURN convert_from(bin, 'utf8');
+    FOR byte IN (select (regexp_matches(input, '(%..|.)', 'g'))[1]) LOOP
+        IF length(byte) = 3 THEN
+            bin = bin || decode(substring(byte, 2, 2), 'hex');
+        ELSE
+            bin = bin || byte::bytea;
+        END IF;
+    END LOOP;
+    RETURN convert_from(bin, 'utf8');
 END
 $$;
 
