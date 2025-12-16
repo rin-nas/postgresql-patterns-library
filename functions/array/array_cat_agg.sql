68fb52613aa309cd8872fedadc7dffb4a7b4f086
Rinat <rin-nas@users.noreply.github.com>
2025-12-16 14:15:14 +0300

    Update array_cat_agg.sql

--- functions/array/array_cat_agg.sql
+++ functions/array/array_cat_agg.sql
@@ -12,7 +12,7 @@ CREATE AGGREGATE public.array_cat_agg(anycompatiblearray) (
    ,INITCOND  = '{}'
 );
 
---TEST
+--TEST1
 SELECT id,  public.array_cat_agg(words::text[])
 FROM (VALUES
              ('1', '{"foo","bar","zap","bing"}'),
@@ -21,4 +21,16 @@ FROM (VALUES
              ('2', '{"bing"}'),
              ('1', '{"bing"}'),
              ('2', '{"foo","bar"}')) AS t(id, words)
+GROUP BY id;
+
+--TEST2 -- без функции array_cat_agg() можно обойтись, если немного переписать запрос!
+SELECT id, array_agg(u.w) as words
+FROM (VALUES
+             ('1', '{"foo","bar","zap","bing"}'),
+             ('2', '{"foo"}'),
+             ('1', '{"bar","zap"}'),
+             ('2', '{"bing"}'),
+             ('1', '{"bing"}'),
+             ('2', '{"foo","bar"}')) AS t(id, words),
+unnest(words::text[]) as u(w)
 GROUP BY id;
