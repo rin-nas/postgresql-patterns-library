3d445ec27c257af0cf35bf7f0fa1c96482fecf50
Rinat <rin-nas@users.noreply.github.com>
2026-02-25 21:52:37 +0300

    Create pg_hba_file.changes_check.sql

---
+++ dev/pg_hba_file.changes_check.sql
@@ -0,0 +1,19 @@
+select
+  md5(s5.content)
+  --, s5.content, s1.content
+from
+  current_setting('hba_file') as cs(hba_file),
+  pg_stat_file(cs.hba_file) as sf,
+  pg_read_file(cs.hba_file) as s1(content),
+  -- remove comments:
+  regexp_replace(s1.content, '#[^\n]*\n', E'\n', 'g') as s2(content),
+  -- remove redundant new lines and spaces around:
+  regexp_replace(s2.content, '\s*\n\s*', E'\n', 'g') as s3(content),
+  -- remove redundant spaces:
+  regexp_replace(s3.content, ' +', ' ', 'g') as s4(content),
+  concat_ws(
+    E'\n',
+    trim(s4.content, E' \n'),
+    sf.modification::text,
+    sf.change::text
+  ) as s5(content);
