b46d852de3d6fc0c794abe4a81bc892902ecd355
Rinat <rin-nas@users.noreply.github.com>
2026-04-13 11:04:13 +0300

    language sql

--- functions/quote_like.sql
+++ functions/quote_like.sql
@@ -5,10 +5,7 @@ create function public.quote_like(text)
     parallel safe -- Postgres 10 or later
     language sql
     set search_path = ''
-as
-$$
-select replace(replace(replace($1
+return replace(replace(replace($1
                                , '\', '\\') -- must come first!
                                , '_', '\_')
                                , '%', '\%');
-$$;
