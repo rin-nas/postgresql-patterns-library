275029944cfb835071257b23be3a2f648b1f62dc
rin-nas <rin-nas@ya.ru>
2026-04-21 11:44:51 +0300

    pg_stat_activity_groups.sql - pid added

--- DBA/pg_stat_activity_groups.sql
+++ DBA/pg_stat_activity_groups.sql
@@ -18,6 +18,9 @@ select
     q."max_query_elapsed.duration",
     t."max_xact_elapsed.duration",
 
+    q."max_query_elapsed.pid",
+    t."max_xact_elapsed.pid",
+
     q."max_query_elapsed.application_name",
     t."max_xact_elapsed.application_name",
 
@@ -29,24 +32,24 @@ left join lateral (
                      then t.state_change - t.query_start
                 else statement_timestamp() - t.query_start
            end as query_elapsed,
+           t.pid,
            t.query,
            t.application_name
     from pg_stat_activity as t
     where (a.backend_type, a.datname, a.state, a.usename, a.wait_event_type, a.wait_event) is not distinct from
           (t.backend_type, t.datname, t.state, t.usename, t.wait_event_type, t.wait_event)
-          and t.query_start is not null
     order by query_elapsed desc
     limit 1
-) as q ("max_query_elapsed.duration", "max_query_elapsed.query", "max_query_elapsed.application_name") on true
+) as q ("max_query_elapsed.duration", "max_query_elapsed.pid", "max_query_elapsed.query", "max_query_elapsed.application_name") on true
 left join lateral (
     select statement_timestamp() - t.xact_start as xact_elapsed,
+           t.pid,
            t.query,
            t.application_name
     from pg_stat_activity as t
     where (a.backend_type, a.datname, a.state, a.usename, a.wait_event_type, a.wait_event) is not distinct from
           (t.backend_type, t.datname, t.state, t.usename, t.wait_event_type, t.wait_event)
-          and t.xact_start is not null
     order by xact_elapsed desc
     limit 1
-) as t ("max_xact_elapsed.duration", "max_xact_elapsed.query", "max_xact_elapsed.application_name") on true
+) as t ("max_xact_elapsed.duration", "max_xact_elapsed.pid", "max_xact_elapsed.query", "max_xact_elapsed.application_name") on true
 order by a;
