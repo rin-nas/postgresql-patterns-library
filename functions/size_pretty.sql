f84bf712effd899d7146b0c33a5ceca42b2be5fa
rin-nas <rin-nas@ya.ru>
2026-08-14 12:18:38 +0300

    func `size_pretty()` added

---
+++ functions/size_pretty.sql
@@ -0,0 +1,28 @@
+create function public.size_pretty(bigint)
+    returns text
+    immutable
+    returns null on null input
+    parallel safe
+    language sql
+    set search_path = ''
+return
+    case when $1 = 0 then '0'
+         else replace(pg_size_pretty($1), ' bytes', ' B')
+    end;
+
+comment on function public.size_pretty(bigint) is 'Formats the size to a human readable string';
+
+
+create function public.size_pretty(numeric)
+    returns text
+    immutable
+    returns null on null input
+    parallel safe
+    language sql
+    set search_path = ''
+return
+    case when $1 = 0 then '0'
+         else replace(pg_size_pretty($1), ' bytes', ' B')
+    end;
+
+comment on function public.size_pretty(numeric) is 'Formats the size to a human readable string';
