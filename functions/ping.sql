727e139f0abfa4487521ea67ec223e3eec42b9fc
rin-nas <rin-nas@ya.ru>
2026-08-22 01:26:43 +0300

    func ping() - remote_server_addr added

--- functions/ping.sql
+++ functions/ping.sql
@@ -2,8 +2,9 @@ create extension if not exists dblink schema public;
 
 create or replace function public.ping(
     connection_str text,
-    latency out interval,
-    time_diff out interval
+    remote_server_addr out inet, -- для подключения по хосту узнаём его IP
+    latency            out interval,
+    time_diff          out interval
 )
     returns record
     immutable
@@ -12,32 +13,37 @@ create or replace function public.ping(
     language sql
     set search_path = ''
 begin atomic
-    select d3.latency,
+    select s.remote_server_addr,
+           d3.latency,
            s.local_connection_start + d3.latency - s.remote_connection_start as time_diff
     from public.dblink(
                ping.connection_str,
                format($sql$
                       select
-                          %L                as local_connection_start,
-                          a.backend_start   as remote_connection_start,
-                          clock_timestamp() as remote_connection_end
+                          %L                 as local_connection_start,
+                          inet_server_addr() as remote_server_addr,
+                          a.backend_start    as remote_connection_start,
+                          clock_timestamp()  as remote_connection_end
                       from pg_stat_activity as a
                       where a.pid = pg_backend_pid()
                       $sql$, clock_timestamp()),
-               false --fail_on_error
+               true --fail_on_error
     ) as s (local_connection_start  timestamptz,
+            remote_server_addr      inet,
             remote_connection_start timestamptz,
-            remote_connection_end   timestamptz),
+            remote_connection_end   timestamptz
+           ),
     coalesce(clock_timestamp() /*local_connection_end*/ - s.local_connection_start) as d2(local_connection_duration),
-    coalesce(s.remote_connection_end - s.remote_connection_start) as d1(remote_connection_duration),
-    coalesce(d2.local_connection_duration - d1.remote_connection_duration) as d3(latency);
+    coalesce(s.remote_connection_end - s.remote_connection_start)                   as d1(remote_connection_duration),
+    coalesce(d2.local_connection_duration - d1.remote_connection_duration)          as d3(latency);
 end;
 
 comment on function public.ping(
     connection_str text,
-    latency out interval,
-    time_diff out interval
-) is 'Get latency and date-time difference between current and remote servers';
+    remote_server_addr out inet,
+    latency            out interval,
+    time_diff          out interval
+) is 'Get remote IP, latency and date-time difference between current and remote servers';
 
 -- TEST
 do $$
