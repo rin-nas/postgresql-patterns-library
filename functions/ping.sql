83d1ea3b607153947ab85b67599138462634f4e7
rin-nas <rin-nas@ya.ru>
2026-08-22 22:19:25 +0300

    `remote_server_addr` renamed to `remote_addr`

--- functions/ping.sql
+++ functions/ping.sql
@@ -2,9 +2,9 @@ create extension if not exists dblink schema public;
 
 create or replace function public.ping(
     connection_str text,
-    remote_server_addr out inet, -- для подключения по хосту узнаём его IP
-    latency            out interval,
-    time_diff          out interval
+    remote_addr out inet, -- для подключения по хосту узнаём его IP
+    latency     out interval,
+    time_diff   out interval
 )
     returns record
     immutable
@@ -13,7 +13,7 @@ create or replace function public.ping(
     language sql
     set search_path = ''
 begin atomic
-    select s.remote_server_addr,
+    select s.remote_addr,
            d3.latency,
            s.local_connection_start + d3.latency - s.remote_connection_start as time_diff
     from public.dblink(
@@ -21,7 +21,7 @@ begin atomic
                format($sql$
                       select
                           %L                 as local_connection_start,
-                          inet_server_addr() as remote_server_addr,
+                          inet_server_addr() as remote_addr,
                           a.backend_start    as remote_connection_start,
                           clock_timestamp()  as remote_connection_end
                       from pg_stat_activity as a
@@ -29,7 +29,7 @@ begin atomic
                       $sql$, clock_timestamp()),
                true --fail_on_error
     ) as s (local_connection_start  timestamptz,
-            remote_server_addr      inet,
+            remote_addr             inet,
             remote_connection_start timestamptz,
             remote_connection_end   timestamptz
            ),
@@ -40,9 +40,9 @@ end;
 
 comment on function public.ping(
     connection_str text,
-    remote_server_addr out inet,
-    latency            out interval,
-    time_diff          out interval
+    remote_addr out inet,
+    latency     out interval,
+    time_diff   out interval
 ) is 'Get remote IP, latency and date-time difference between current and remote servers';
 
 -- TEST
